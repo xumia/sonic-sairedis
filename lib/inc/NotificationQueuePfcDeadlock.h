dafa65f3a8da61f3f4206acc99cd387678fb2c9f
kcudnik <kcudnik@gmail.com>
2019-12-10 19:48:26 +0100

    [sairedis] Add NotificationQueuePfcDeadlock class

---
+++ lib/inc/NotificationQueuePfcDeadlock.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "Notification.h"
+
+namespace sairedis
+{
+    class NotificationQueuePfcDeadlock:
+        public Notification
+    {
+        public:
+
+            NotificationQueuePfcDeadlock(
+                    _In_ const std::string& serializedNotification);
+
+            virtual ~NotificationQueuePfcDeadlock();
+
+        public:
+
+            virtual sai_object_id_t getSwitchId() const override;
+
+            virtual sai_object_id_t getAnyObjectId() const override;
+
+            virtual void processMetadata() const override;
+
+            virtual void executeCallback(
+                    _In_ const sai_switch_notifications_t& switchNotifications) const override;
+
+        private:
+
+            uint32_t m_count;
+
+            sai_queue_deadlock_notification_data_t *m_queueDeadlockNotificationData;
+    };
+}
+
---
+++ lib/src/NotificationQueuePfcDeadlock.cpp
@@ -0,0 +1,80 @@
+#include "NotificationQueuePfcDeadlock.h"
+
+#include "swss/logger.h"
+
+#include "meta/sai_serialize.h"
+#include "meta/sai_meta.h"
+
+using namespace sairedis;
+
+NotificationQueuePfcDeadlock::NotificationQueuePfcDeadlock(
+        _In_ const std::string& serializedNotification):
+    Notification(
+            SAI_SWITCH_NOTIFICATION_TYPE_QUEUE_PFC_DEADLOCK,
+            serializedNotification),
+    m_queueDeadlockNotificationData(nullptr)
+{
+    SWSS_LOG_ENTER();
+
+    sai_deserialize_queue_deadlock_ntf(
+            serializedNotification,
+            m_count,
+            &m_queueDeadlockNotificationData);
+}
+
+NotificationQueuePfcDeadlock::~NotificationQueuePfcDeadlock()
+{
+    SWSS_LOG_ENTER();
+
+    sai_deserialize_free_queue_deadlock_ntf(m_count, m_queueDeadlockNotificationData);
+}
+
+sai_object_id_t NotificationQueuePfcDeadlock::getSwitchId() const
+{
+    SWSS_LOG_ENTER();
+
+    // notification don't have switch id
+    
+    return SAI_NULL_OBJECT_ID;
+}
+
+sai_object_id_t NotificationQueuePfcDeadlock::getAnyObjectId() const
+{
+    SWSS_LOG_ENTER();
+
+    if (m_queueDeadlockNotificationData == nullptr)
+    {
+        return SAI_NULL_OBJECT_ID;
+    }
+
+    for (uint32_t idx = 0; idx < m_count; idx++)
+    {
+        if (m_queueDeadlockNotificationData[idx].queue_id != SAI_NULL_OBJECT_ID)
+        {
+            return m_queueDeadlockNotificationData[idx].queue_id;
+        }
+    }
+
+    return SAI_NULL_OBJECT_ID;
+}
+
+void NotificationQueuePfcDeadlock::processMetadata() const
+{
+    SWSS_LOG_ENTER();
+
+    // TODO add parameter with metadata object, currently we are calling global
+    // functions
+
+    meta_sai_on_queue_pfc_deadlock_notification(m_count, m_queueDeadlockNotificationData);
+}
+
+void NotificationQueuePfcDeadlock::executeCallback(
+        _In_ const sai_switch_notifications_t& switchNotifications) const
+{
+    SWSS_LOG_ENTER();
+
+    if (switchNotifications.on_queue_pfc_deadlock)
+    {
+        switchNotifications.on_queue_pfc_deadlock(m_count, m_queueDeadlockNotificationData);
+    }
+}
