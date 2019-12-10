93e026180269a859eb607cd551292ae8a04bdd97
kcudnik <kcudnik@gmail.com>
2019-12-10 19:48:11 +0100

    [sairedis] Add NotificationPortStateChange class

---
+++ lib/inc/NotificationPortStateChange.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "Notification.h"
+
+namespace sairedis
+{
+    class NotificationPortStateChange:
+        public Notification
+    {
+        public:
+
+            NotificationPortStateChange(
+                    _In_ const std::string& serializedNotification);
+
+            virtual ~NotificationPortStateChange();
+
+        public:
+
+            virtual sai_object_id_t getSwitchId() const override;
+
+            virtual sai_object_id_t getAnyObjectId() const override;
+
+            virtual void processMetadata() const override;
+
+            virtual void executeCallback(
+                    _In_ const sai_switch_notifications_t& switchNotifications) const override;
+
+        private:
+
+            uint32_t m_count;
+
+            sai_port_oper_status_notification_t *m_portOperaStatusNotificationData;
+    };
+}
---
+++ lib/src/NotificationPortStateChange.cpp
@@ -0,0 +1,80 @@
+#include "NotificationPortStateChange.h"
+
+#include "swss/logger.h"
+
+#include "meta/sai_serialize.h"
+#include "meta/sai_meta.h"
+
+using namespace sairedis;
+
+NotificationPortStateChange::NotificationPortStateChange(
+        _In_ const std::string& serializedNotification):
+    Notification(
+            SAI_SWITCH_NOTIFICATION_TYPE_PORT_STATE_CHANGE,
+            serializedNotification),
+    m_portOperaStatusNotificationData(nullptr)
+{
+    SWSS_LOG_ENTER();
+
+    sai_deserialize_port_oper_status_ntf(
+            serializedNotification,
+            m_count, 
+            &m_portOperaStatusNotificationData);
+}
+
+NotificationPortStateChange::~NotificationPortStateChange()
+{
+    SWSS_LOG_ENTER();
+
+    sai_deserialize_free_port_oper_status_ntf(m_count, m_portOperaStatusNotificationData);
+}
+
+sai_object_id_t NotificationPortStateChange::getSwitchId() const
+{
+    SWSS_LOG_ENTER();
+
+    // this notification don't contain switch id field
+
+    return SAI_NULL_OBJECT_ID;
+}
+
+sai_object_id_t NotificationPortStateChange::getAnyObjectId() const
+{
+    SWSS_LOG_ENTER();
+
+    if (m_portOperaStatusNotificationData == nullptr)
+    {
+        return SAI_NULL_OBJECT_ID;
+    }
+
+    for (uint32_t idx = 0; idx < m_count; idx++)
+    {
+        if (m_portOperaStatusNotificationData[idx].port_id != SAI_NULL_OBJECT_ID)
+        {
+            return m_portOperaStatusNotificationData[idx].port_id;
+        }
+    }
+
+    return SAI_NULL_OBJECT_ID;
+}
+
+void NotificationPortStateChange::processMetadata() const
+{
+    SWSS_LOG_ENTER();
+
+    // TODO add parameter with metadata object, currently we are calling global
+    // functions
+
+    meta_sai_on_port_state_change(m_count, m_portOperaStatusNotificationData);
+}
+
+void NotificationPortStateChange::executeCallback(
+        _In_ const sai_switch_notifications_t& switchNotifications) const
+{
+    SWSS_LOG_ENTER();
+
+    if (switchNotifications.on_port_state_change)
+    {
+        switchNotifications.on_port_state_change(m_count, m_portOperaStatusNotificationData);
+    }
+}
