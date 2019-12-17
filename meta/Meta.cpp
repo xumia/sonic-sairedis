cc9ee4fe14629189c5a4e1f66fc592a968dbc53c
kcudnik <kcudnik@gmail.com>
2019-12-17 20:43:21 +0100

    [saimeta] Add non object (*_entry) ids remove methods

--- meta/Meta.cpp
+++ meta/Meta.cpp
@@ -19,6 +19,40 @@ sai_status_t meta_sai_validate_oid(
 void meta_generic_validation_post_remove(
         _In_ const sai_object_meta_key_t& meta_key);
 
+sai_status_t meta_sai_validate_fdb_entry(
+        _In_ const sai_fdb_entry_t* fdb_entry,
+        _In_ bool create,
+        _In_ bool get = false);
+
+sai_status_t meta_sai_validate_mcast_fdb_entry(
+        _In_ const sai_mcast_fdb_entry_t* mcast_fdb_entry,
+        _In_ bool create,
+        _In_ bool get = false);
+
+sai_status_t meta_sai_validate_neighbor_entry(
+        _In_ const sai_neighbor_entry_t* neighbor_entry,
+        _In_ bool create);
+
+sai_status_t meta_sai_validate_route_entry(
+        _In_ const sai_route_entry_t* route_entry,
+        _In_ bool create);
+
+sai_status_t meta_sai_validate_l2mc_entry(
+        _In_ const sai_l2mc_entry_t* l2mc_entry,
+        _In_ bool create);
+
+sai_status_t meta_sai_validate_ipmc_entry(
+        _In_ const sai_ipmc_entry_t* ipmc_entry,
+        _In_ bool create);
+
+sai_status_t meta_sai_validate_nat_entry(
+        _In_ const sai_nat_entry_t* nat_entry,
+        _In_ bool create);
+
+sai_status_t meta_sai_validate_inseg_entry(
+        _In_ const sai_inseg_entry_t* inseg_entry,
+        _In_ bool create);
+
 sai_status_t Meta::remove(
         _In_ sai_object_type_t object_type,
         _In_ sai_object_id_t object_id,
@@ -43,6 +77,334 @@ sai_status_t Meta::remove(
     }
 
     status = saiInterface.remove(object_type, object_id);
+
+    if (status == SAI_STATUS_SUCCESS)
+    {
+        SWSS_LOG_DEBUG("remove status: %s", sai_serialize_status(status).c_str());
+    }
+    else
+    {
+        SWSS_LOG_ERROR("remove status: %s", sai_serialize_status(status).c_str());
+    }
+
+    if (status == SAI_STATUS_SUCCESS)
+    {
+        meta_generic_validation_post_remove(meta_key);
+    }
+
+    return status;
+}
+
+sai_status_t Meta::remove(
+        _In_ const sai_fdb_entry_t* fdb_entry,
+        _Inout_ sairedis::SaiInterface& saiInterface)
+{
+    SWSS_LOG_ENTER();
+
+    sai_status_t status = meta_sai_validate_fdb_entry(fdb_entry, false);
+
+    if (status != SAI_STATUS_SUCCESS)
+    {
+        return status;
+    }
+
+    sai_object_meta_key_t meta_key = { .objecttype = SAI_OBJECT_TYPE_FDB_ENTRY, .objectkey = { .key = { .fdb_entry = *fdb_entry  } } };
+
+    status = meta_generic_validation_remove(meta_key);
+
+    if (status != SAI_STATUS_SUCCESS)
+    {
+        return status;
+    }
+
+    status = saiInterface.remove(fdb_entry);
+
+    if (status == SAI_STATUS_SUCCESS)
+    {
+        SWSS_LOG_DEBUG("remove status: %s", sai_serialize_status(status).c_str());
+    }
+    else
+    {
+        SWSS_LOG_ERROR("remove status: %s", sai_serialize_status(status).c_str());
+    }
+
+    if (status == SAI_STATUS_SUCCESS)
+    {
+        meta_generic_validation_post_remove(meta_key);
+    }
+
+    return status;
+}
+
+sai_status_t Meta::remove(
+        _In_ const sai_mcast_fdb_entry_t* mcast_fdb_entry,
+        _Inout_ sairedis::SaiInterface& saiInterface)
+{
+    SWSS_LOG_ENTER();
+
+    sai_status_t status = meta_sai_validate_mcast_fdb_entry(mcast_fdb_entry, false);
+
+    if (status != SAI_STATUS_SUCCESS)
+    {
+        return status;
+    }
+
+    sai_object_meta_key_t meta_key = { .objecttype = SAI_OBJECT_TYPE_MCAST_FDB_ENTRY, .objectkey = { .key = { .mcast_fdb_entry = *mcast_fdb_entry  } } };
+
+    status = meta_generic_validation_remove(meta_key);
+
+    if (status != SAI_STATUS_SUCCESS)
+    {
+        return status;
+    }
+
+    status = saiInterface.remove(mcast_fdb_entry);
+
+    if (status == SAI_STATUS_SUCCESS)
+    {
+        SWSS_LOG_DEBUG("remove status: %s", sai_serialize_status(status).c_str());
+    }
+    else
+    {
+        SWSS_LOG_ERROR("remove status: %s", sai_serialize_status(status).c_str());
+    }
+
+    if (status == SAI_STATUS_SUCCESS)
+    {
+        meta_generic_validation_post_remove(meta_key);
+    }
+
+    return status;
+}
+
+sai_status_t Meta::remove(
+        _In_ const sai_neighbor_entry_t* neighbor_entry,
+        _Inout_ sairedis::SaiInterface& saiInterface)
+{
+    SWSS_LOG_ENTER();
+
+    sai_status_t status = meta_sai_validate_neighbor_entry(neighbor_entry, false);
+
+    if (status != SAI_STATUS_SUCCESS)
+    {
+        return status;
+    }
+
+    sai_object_meta_key_t meta_key = { .objecttype = SAI_OBJECT_TYPE_NEIGHBOR_ENTRY, .objectkey = { .key = { .neighbor_entry = *neighbor_entry  } } };
+
+    status = meta_generic_validation_remove(meta_key);
+
+    if (status != SAI_STATUS_SUCCESS)
+    {
+        return status;
+    }
+
+    status = saiInterface.remove(neighbor_entry);
+
+    if (status == SAI_STATUS_SUCCESS)
+    {
+        SWSS_LOG_DEBUG("remove status: %s", sai_serialize_status(status).c_str());
+    }
+    else
+    {
+        SWSS_LOG_ERROR("remove status: %s", sai_serialize_status(status).c_str());
+    }
+
+    if (status == SAI_STATUS_SUCCESS)
+    {
+        meta_generic_validation_post_remove(meta_key);
+    }
+
+    return status;
+}
+
+sai_status_t Meta::remove(
+        _In_ const sai_route_entry_t* route_entry,
+        _Inout_ sairedis::SaiInterface& saiInterface)
+{
+    SWSS_LOG_ENTER();
+
+    sai_status_t status = meta_sai_validate_route_entry(route_entry, false);
+
+    if (status != SAI_STATUS_SUCCESS)
+    {
+        return status;
+    }
+
+    sai_object_meta_key_t meta_key = { .objecttype = SAI_OBJECT_TYPE_ROUTE_ENTRY, .objectkey = { .key = { .route_entry = *route_entry  } } };
+
+    status = meta_generic_validation_remove(meta_key);
+
+    if (status != SAI_STATUS_SUCCESS)
+    {
+        return status;
+    }
+
+    status = saiInterface.remove(route_entry);
+
+    if (status == SAI_STATUS_SUCCESS)
+    {
+        SWSS_LOG_DEBUG("remove status: %s", sai_serialize_status(status).c_str());
+    }
+    else
+    {
+        SWSS_LOG_ERROR("remove status: %s", sai_serialize_status(status).c_str());
+    }
+
+    if (status == SAI_STATUS_SUCCESS)
+    {
+        meta_generic_validation_post_remove(meta_key);
+    }
+
+    return status;
+}
+
+sai_status_t Meta::remove(
+        _In_ const sai_l2mc_entry_t* l2mc_entry,
+        _Inout_ sairedis::SaiInterface& saiInterface)
+{
+    SWSS_LOG_ENTER();
+
+    sai_status_t status = meta_sai_validate_l2mc_entry(l2mc_entry, false);
+
+    if (status != SAI_STATUS_SUCCESS)
+    {
+        return status;
+    }
+
+    sai_object_meta_key_t meta_key = { .objecttype = SAI_OBJECT_TYPE_L2MC_ENTRY, .objectkey = { .key = { .l2mc_entry = *l2mc_entry  } } };
+
+    status = meta_generic_validation_remove(meta_key);
+
+    if (status != SAI_STATUS_SUCCESS)
+    {
+        return status;
+    }
+
+    status = saiInterface.remove(l2mc_entry);
+
+    if (status == SAI_STATUS_SUCCESS)
+    {
+        SWSS_LOG_DEBUG("remove status: %s", sai_serialize_status(status).c_str());
+    }
+    else
+    {
+        SWSS_LOG_ERROR("remove status: %s", sai_serialize_status(status).c_str());
+    }
+
+    if (status == SAI_STATUS_SUCCESS)
+    {
+        meta_generic_validation_post_remove(meta_key);
+    }
+
+    return status;
+}
+
+sai_status_t Meta::remove(
+        _In_ const sai_ipmc_entry_t* ipmc_entry,
+        _Inout_ sairedis::SaiInterface& saiInterface)
+{
+    SWSS_LOG_ENTER();
+
+    sai_status_t status = meta_sai_validate_ipmc_entry(ipmc_entry, false);
+
+    if (status != SAI_STATUS_SUCCESS)
+    {
+        return status;
+    }
+
+    sai_object_meta_key_t meta_key = { .objecttype = SAI_OBJECT_TYPE_IPMC_ENTRY, .objectkey = { .key = { .ipmc_entry = *ipmc_entry  } } };
+
+    status = meta_generic_validation_remove(meta_key);
+
+    if (status != SAI_STATUS_SUCCESS)
+    {
+        return status;
+    }
+
+    status = saiInterface.remove(ipmc_entry);
+
+    if (status == SAI_STATUS_SUCCESS)
+    {
+        SWSS_LOG_DEBUG("remove status: %s", sai_serialize_status(status).c_str());
+    }
+    else
+    {
+        SWSS_LOG_ERROR("remove status: %s", sai_serialize_status(status).c_str());
+    }
+
+    if (status == SAI_STATUS_SUCCESS)
+    {
+        meta_generic_validation_post_remove(meta_key);
+    }
+
+    return status;
+}
+
+sai_status_t Meta::remove(
+        _In_ const sai_nat_entry_t* nat_entry,
+        _Inout_ sairedis::SaiInterface& saiInterface)
+{
+    SWSS_LOG_ENTER();
+
+    sai_status_t status = meta_sai_validate_nat_entry(nat_entry, false);
+
+    if (status != SAI_STATUS_SUCCESS)
+    {
+        return status;
+    }
+
+    sai_object_meta_key_t meta_key = { .objecttype = SAI_OBJECT_TYPE_NAT_ENTRY, .objectkey = { .key = { .nat_entry = *nat_entry  } } };
+
+    status = meta_generic_validation_remove(meta_key);
+
+    if (status != SAI_STATUS_SUCCESS)
+    {
+        return status;
+    }
+
+    status = saiInterface.remove(nat_entry);
+
+    if (status == SAI_STATUS_SUCCESS)
+    {
+        SWSS_LOG_DEBUG("remove status: %s", sai_serialize_status(status).c_str());
+    }
+    else
+    {
+        SWSS_LOG_ERROR("remove status: %s", sai_serialize_status(status).c_str());
+    }
+
+    if (status == SAI_STATUS_SUCCESS)
+    {
+      meta_generic_validation_post_remove(meta_key);
+    }
+
+    return status;
+}
+
+sai_status_t Meta::remove(
+        _In_ const sai_inseg_entry_t* inseg_entry,
+        _Inout_ sairedis::SaiInterface& saiInterface)
+{
+    SWSS_LOG_ENTER();
+
+    sai_status_t status = meta_sai_validate_inseg_entry(inseg_entry, false);
+
+    if (status != SAI_STATUS_SUCCESS)
+    {
+        return status;
+    }
+
+    sai_object_meta_key_t meta_key = { .objecttype = SAI_OBJECT_TYPE_INSEG_ENTRY, .objectkey = { .key = { .inseg_entry = *inseg_entry  } } };
+
+    status = meta_generic_validation_remove(meta_key);
+
+    if (status != SAI_STATUS_SUCCESS)
+    {
+        return status;
+    }
+
+    status = saiInterface.remove(inseg_entry);
 
     if (status == SAI_STATUS_SUCCESS)
     {
--- meta/Meta.h
+++ meta/Meta.h
@@ -2,6 +2,11 @@
 
 #include "lib/inc/SaiInterface.h"
 
+#define SAIMETA_META_DECLARE_REMOVE_ENTRY(ot)                   \
+    sai_status_t remove(                                        \
+            _In_ const sai_ ## ot ## _t* ot,                    \
+            _Inout_ sairedis::SaiInterface& saiInterface);
+
 namespace saimeta
 {
     class Meta
@@ -18,5 +23,15 @@ namespace saimeta
                     _In_ sai_object_type_t object_type,
                     _In_ sai_object_id_t object_id,
                     _Inout_ sairedis::SaiInterface& saiInterface);
+
+            SAIMETA_META_DECLARE_REMOVE_ENTRY(fdb_entry);
+            SAIMETA_META_DECLARE_REMOVE_ENTRY(inseg_entry);
+            SAIMETA_META_DECLARE_REMOVE_ENTRY(ipmc_entry);
+            SAIMETA_META_DECLARE_REMOVE_ENTRY(l2mc_entry);
+            SAIMETA_META_DECLARE_REMOVE_ENTRY(mcast_fdb_entry);
+            SAIMETA_META_DECLARE_REMOVE_ENTRY(neighbor_entry);
+            SAIMETA_META_DECLARE_REMOVE_ENTRY(route_entry);
+            SAIMETA_META_DECLARE_REMOVE_ENTRY(nat_entry);
+
     };
 };
