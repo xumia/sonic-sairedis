06cdc30f459eaf8adcb165547dcca8c475add0eb
kcudnik <kcudnik@gmail.com>
2019-12-17 22:22:06 +0100

    [saimeta] Add non object (*_entry) ids create methods

--- meta/Meta.cpp
+++ meta/Meta.cpp
@@ -53,6 +53,12 @@ sai_status_t meta_sai_validate_inseg_entry(
         _In_ const sai_inseg_entry_t* inseg_entry,
         _In_ bool create);
 
+sai_status_t meta_generic_validation_create(
+        _In_ const sai_object_meta_key_t& meta_key,
+        _In_ sai_object_id_t switch_id,
+        _In_ const uint32_t attr_count,
+        _In_ const sai_attribute_t *attr_list);
+
 sai_status_t Meta::remove(
         _In_ sai_object_type_t object_type,
         _In_ sai_object_id_t object_id,
@@ -418,6 +424,349 @@ sai_status_t Meta::remove(
     if (status == SAI_STATUS_SUCCESS)
     {
         meta_generic_validation_post_remove(meta_key);
+    }
+
+    return status;
+}
+
+sai_status_t Meta::create(
+        _In_ const sai_fdb_entry_t* fdb_entry,
+        _In_ uint32_t attr_count,
+        _In_ const sai_attribute_t *attr_list,
+        _Inout_ sairedis::SaiInterface& saiInterface)
+{
+    SWSS_LOG_ENTER();
+
+    sai_status_t status = meta_sai_validate_fdb_entry(fdb_entry, true);
+
+    if (status != SAI_STATUS_SUCCESS)
+    {
+        return status;
+    }
+
+    sai_object_meta_key_t meta_key = { .objecttype = SAI_OBJECT_TYPE_FDB_ENTRY, .objectkey = { .key = { .fdb_entry = *fdb_entry  } } };
+
+    status = meta_generic_validation_create(meta_key, fdb_entry->switch_id, attr_count, attr_list);
+
+    if (status != SAI_STATUS_SUCCESS)
+    {
+        return status;
+    }
+
+    status = saiInterface.create(fdb_entry, attr_count, attr_list);
+
+    if (status == SAI_STATUS_SUCCESS)
+    {
+        SWSS_LOG_DEBUG("create status: %s", sai_serialize_status(status).c_str());
+    }
+    else
+    {
+        SWSS_LOG_ERROR("create status: %s", sai_serialize_status(status).c_str());
+    }
+
+    if (status == SAI_STATUS_SUCCESS)
+    {
+        meta_generic_validation_post_create(meta_key, fdb_entry->switch_id, attr_count, attr_list);
+    }
+
+    return status;
+}
+
+sai_status_t Meta::create(
+        _In_ const sai_mcast_fdb_entry_t* mcast_fdb_entry,
+        _In_ uint32_t attr_count,
+        _In_ const sai_attribute_t *attr_list,
+        _Inout_ sairedis::SaiInterface& saiInterface)
+{
+    SWSS_LOG_ENTER();
+
+    sai_status_t status = meta_sai_validate_mcast_fdb_entry(mcast_fdb_entry, true);
+
+    if (status != SAI_STATUS_SUCCESS)
+    {
+        return status;
+    }
+
+    sai_object_meta_key_t meta_key = { .objecttype = SAI_OBJECT_TYPE_MCAST_FDB_ENTRY, .objectkey = { .key = { .mcast_fdb_entry = *mcast_fdb_entry  } } };
+
+    status = meta_generic_validation_create(meta_key, mcast_fdb_entry->switch_id, attr_count, attr_list);
+
+    if (status != SAI_STATUS_SUCCESS)
+    {
+        return status;
+    }
+
+    status = saiInterface.create(mcast_fdb_entry, attr_count, attr_list);
+
+    if (status == SAI_STATUS_SUCCESS)
+    {
+        SWSS_LOG_DEBUG("create status: %s", sai_serialize_status(status).c_str());
+    }
+    else
+    {
+        SWSS_LOG_ERROR("create status: %s", sai_serialize_status(status).c_str());
+    }
+
+    if (status == SAI_STATUS_SUCCESS)
+    {
+        meta_generic_validation_post_create(meta_key, mcast_fdb_entry->switch_id, attr_count, attr_list);
+    }
+
+    return status;
+}
+
+sai_status_t Meta::create(
+        _In_ const sai_neighbor_entry_t* neighbor_entry,
+        _In_ uint32_t attr_count,
+        _In_ const sai_attribute_t *attr_list,
+        _Inout_ sairedis::SaiInterface& saiInterface)
+{
+    SWSS_LOG_ENTER();
+
+    sai_status_t status = meta_sai_validate_neighbor_entry(neighbor_entry, true);
+
+    if (status != SAI_STATUS_SUCCESS)
+    {
+        return status;
+    }
+
+    sai_object_meta_key_t meta_key = { .objecttype = SAI_OBJECT_TYPE_NEIGHBOR_ENTRY, .objectkey = { .key = { .neighbor_entry = *neighbor_entry  } } };
+
+    status = meta_generic_validation_create(meta_key, neighbor_entry->switch_id, attr_count, attr_list);
+
+    if (status != SAI_STATUS_SUCCESS)
+    {
+        return status;
+    }
+
+    status = saiInterface.create(neighbor_entry, attr_count, attr_list);
+
+    if (status == SAI_STATUS_SUCCESS)
+    {
+        SWSS_LOG_DEBUG("create status: %s", sai_serialize_status(status).c_str());
+    }
+    else
+    {
+        SWSS_LOG_ERROR("create status: %s", sai_serialize_status(status).c_str());
+    }
+
+    if (status == SAI_STATUS_SUCCESS)
+    {
+        meta_generic_validation_post_create(meta_key, neighbor_entry->switch_id, attr_count, attr_list);
+    }
+
+    return status;
+}
+sai_status_t Meta::create(
+        _In_ const sai_route_entry_t* route_entry,
+        _In_ uint32_t attr_count,
+        _In_ const sai_attribute_t *attr_list,
+        _Inout_ sairedis::SaiInterface& saiInterface)
+{
+    SWSS_LOG_ENTER();
+
+    sai_status_t status = meta_sai_validate_route_entry(route_entry, true);
+
+    if (status != SAI_STATUS_SUCCESS)
+    {
+        return status;
+    }
+
+    sai_object_meta_key_t meta_key = { .objecttype = SAI_OBJECT_TYPE_ROUTE_ENTRY, .objectkey = { .key = { .route_entry = *route_entry  } } };
+
+    status = meta_generic_validation_create(meta_key, route_entry->switch_id, attr_count, attr_list);
+
+    if (status != SAI_STATUS_SUCCESS)
+    {
+        return status;
+    }
+
+    status = saiInterface.create(route_entry, attr_count, attr_list);
+
+    if (status == SAI_STATUS_SUCCESS)
+    {
+        SWSS_LOG_DEBUG("create status: %s", sai_serialize_status(status).c_str());
+    }
+    else
+    {
+        SWSS_LOG_ERROR("create status: %s", sai_serialize_status(status).c_str());
+    }
+
+    if (status == SAI_STATUS_SUCCESS)
+    {
+        meta_generic_validation_post_create(meta_key, route_entry->switch_id, attr_count, attr_list);
+    }
+
+    return status;
+}
+
+sai_status_t Meta::create(
+        _In_ const sai_l2mc_entry_t* l2mc_entry,
+        _In_ uint32_t attr_count,
+        _In_ const sai_attribute_t *attr_list,
+        _Inout_ sairedis::SaiInterface& saiInterface)
+{
+    SWSS_LOG_ENTER();
+
+    sai_status_t status = meta_sai_validate_l2mc_entry(l2mc_entry, true);
+
+    if (status != SAI_STATUS_SUCCESS)
+    {
+        return status;
+    }
+
+    sai_object_meta_key_t meta_key = { .objecttype = SAI_OBJECT_TYPE_L2MC_ENTRY, .objectkey = { .key = { .l2mc_entry = *l2mc_entry  } } };
+
+    status = meta_generic_validation_create(meta_key, l2mc_entry->switch_id, attr_count, attr_list);
+
+    if (status != SAI_STATUS_SUCCESS)
+    {
+        return status;
+    }
+
+    status = saiInterface.create(l2mc_entry, attr_count, attr_list);
+
+    if (status == SAI_STATUS_SUCCESS)
+    {
+        SWSS_LOG_DEBUG("create status: %s", sai_serialize_status(status).c_str());
+    }
+    else
+    {
+        SWSS_LOG_ERROR("create status: %s", sai_serialize_status(status).c_str());
+    }
+
+    if (status == SAI_STATUS_SUCCESS)
+    {
+        meta_generic_validation_post_create(meta_key, l2mc_entry->switch_id, attr_count, attr_list);
+    }
+
+    return status;
+}
+
+sai_status_t Meta::create(
+        _In_ const sai_ipmc_entry_t* ipmc_entry,
+        _In_ uint32_t attr_count,
+        _In_ const sai_attribute_t *attr_list,
+        _Inout_ sairedis::SaiInterface& saiInterface)
+{
+    SWSS_LOG_ENTER();
+
+    sai_status_t status = meta_sai_validate_ipmc_entry(ipmc_entry, true);
+
+    if (status != SAI_STATUS_SUCCESS)
+    {
+        return status;
+    }
+
+    sai_object_meta_key_t meta_key = { .objecttype = SAI_OBJECT_TYPE_IPMC_ENTRY, .objectkey = { .key = { .ipmc_entry = *ipmc_entry  } } };
+
+    status = meta_generic_validation_create(meta_key, ipmc_entry->switch_id, attr_count, attr_list);
+
+    if (status != SAI_STATUS_SUCCESS)
+    {
+        return status;
+    }
+
+    status = saiInterface.create(ipmc_entry, attr_count, attr_list);
+
+    if (status == SAI_STATUS_SUCCESS)
+    {
+        SWSS_LOG_DEBUG("create status: %s", sai_serialize_status(status).c_str());
+    }
+    else
+    {
+        SWSS_LOG_ERROR("create status: %s", sai_serialize_status(status).c_str());
+    }
+
+    if (status == SAI_STATUS_SUCCESS)
+    {
+        meta_generic_validation_post_create(meta_key, ipmc_entry->switch_id, attr_count, attr_list);
+    }
+
+    return status;
+}
+
+sai_status_t Meta::create(
+        _In_ const sai_inseg_entry_t* inseg_entry,
+        _In_ uint32_t attr_count,
+        _In_ const sai_attribute_t *attr_list,
+        _Inout_ sairedis::SaiInterface& saiInterface)
+{
+    SWSS_LOG_ENTER();
+
+    sai_status_t status = meta_sai_validate_inseg_entry(inseg_entry, true);
+
+    if (status != SAI_STATUS_SUCCESS)
+    {
+        return status;
+    }
+
+    sai_object_meta_key_t meta_key = { .objecttype = SAI_OBJECT_TYPE_INSEG_ENTRY, .objectkey = { .key = { .inseg_entry = *inseg_entry  } } };
+
+    status = meta_generic_validation_create(meta_key, inseg_entry->switch_id, attr_count, attr_list);
+
+    if (status != SAI_STATUS_SUCCESS)
+    {
+        return status;
+    }
+
+    status = saiInterface.create(inseg_entry, attr_count, attr_list);
+
+    if (status == SAI_STATUS_SUCCESS)
+    {
+        SWSS_LOG_DEBUG("create status: %s", sai_serialize_status(status).c_str());
+    }
+    else
+    {
+        SWSS_LOG_ERROR("create status: %s", sai_serialize_status(status).c_str());
+    }
+
+    if (status == SAI_STATUS_SUCCESS)
+    {
+        meta_generic_validation_post_create(meta_key, inseg_entry->switch_id, attr_count, attr_list);
+    }
+
+    return status;
+}
+
+sai_status_t Meta::create(
+        _In_ const sai_nat_entry_t* nat_entry,
+        _In_ uint32_t attr_count,
+        _In_ const sai_attribute_t *attr_list,
+        _Inout_ sairedis::SaiInterface& saiInterface)
+{
+    SWSS_LOG_ENTER();
+
+    sai_status_t status = meta_sai_validate_nat_entry(nat_entry, true);
+
+    if (status != SAI_STATUS_SUCCESS)
+    {
+        return status;
+    }
+
+    sai_object_meta_key_t meta_key = { .objecttype = SAI_OBJECT_TYPE_NAT_ENTRY, .objectkey = { .key = { .nat_entry = *nat_entry  } } };
+
+    status = meta_generic_validation_create(meta_key, nat_entry->switch_id, attr_count, attr_list);
+
+    if (status != SAI_STATUS_SUCCESS)
+    {
+        return status;
+    }
+
+    status = saiInterface.create(nat_entry, attr_count, attr_list);
+
+    if (status == SAI_STATUS_SUCCESS)
+    {
+        SWSS_LOG_DEBUG("create status: %s", sai_serialize_status(status).c_str());
+    }
+    else
+    {
+        SWSS_LOG_ERROR("create status: %s", sai_serialize_status(status).c_str());
+    }
+
+    if (status == SAI_STATUS_SUCCESS)
+    {
+        meta_generic_validation_post_create(meta_key, nat_entry->switch_id, attr_count, attr_list);
     }
 
     return status;
--- meta/Meta.h
+++ meta/Meta.h
@@ -7,6 +7,13 @@
             _In_ const sai_ ## ot ## _t* ot,                    \
             _Inout_ sairedis::SaiInterface& saiInterface);
 
+#define SAIMETA_META_DECLARE_CREATE_ENTRY(ot)                   \
+    sai_status_t create(                                        \
+            _In_ const sai_ ## ot ## _t* ot,                    \
+            _In_ uint32_t attr_count,                           \
+            _In_ const sai_attribute_t *attr_list,              \
+            _Inout_ sairedis::SaiInterface& saiInterface);
+
 namespace saimeta
 {
     class Meta
@@ -23,6 +30,19 @@ namespace saimeta
                     _In_ sai_object_type_t object_type,
                     _In_ sai_object_id_t object_id,
                     _Inout_ sairedis::SaiInterface& saiInterface);
+
+        public: // create ENTRY
+
+            SAIMETA_META_DECLARE_CREATE_ENTRY(fdb_entry);
+            SAIMETA_META_DECLARE_CREATE_ENTRY(inseg_entry);
+            SAIMETA_META_DECLARE_CREATE_ENTRY(ipmc_entry);
+            SAIMETA_META_DECLARE_CREATE_ENTRY(l2mc_entry);
+            SAIMETA_META_DECLARE_CREATE_ENTRY(mcast_fdb_entry);
+            SAIMETA_META_DECLARE_CREATE_ENTRY(neighbor_entry);
+            SAIMETA_META_DECLARE_CREATE_ENTRY(route_entry);
+            SAIMETA_META_DECLARE_CREATE_ENTRY(nat_entry);
+
+        public: // remove ENTRY
 
             SAIMETA_META_DECLARE_REMOVE_ENTRY(fdb_entry);
             SAIMETA_META_DECLARE_REMOVE_ENTRY(inseg_entry);
