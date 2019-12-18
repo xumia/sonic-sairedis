9bca1eba00670e6702d3f909d10aa7467b1140c1
kcudnik <kcudnik@gmail.com>
2019-12-18 20:30:16 +0100

    [sairedis] Remove unused code

--- lib/src/sai_redis_generic_remove.cpp
+++ lib/src/sai_redis_generic_remove.cpp
@@ -2,56 +2,6 @@
 #include "meta/sai_serialize.h"
 #include "meta/saiattributelist.h"
 
-//sai_status_t internal_redis_generic_remove(
-//        _In_ sai_object_type_t object_type,
-//        _In_ const std::string &serialized_object_id)
-//{
-//    SWSS_LOG_ENTER();
-//
-//    std::string str_object_type = sai_serialize_object_type(object_type);
-//
-//    std::string key = str_object_type + ":" + serialized_object_id;
-//
-//    SWSS_LOG_DEBUG("generic remove key: %s", key.c_str());
-//
-//    g_recorder->recordGenericRemove(key);
-//
-//    g_asicState->del(key, "remove");
-//
-//    auto status = internal_api_wait_for_response(SAI_COMMON_API_REMOVE);
-//
-//    g_recorder->recordGenericRemoveResponse(status);
-//
-//    return status;
-//}
-
-//sai_status_t redis_generic_remove(
-//        _In_ sai_object_type_t object_type,
-//        _In_ sai_object_id_t object_id)
-//{
-//    SWSS_LOG_ENTER();
-//
-//    g_recorder->recordGenericRemove(object_type, object_id);
-//
-//    auto status = g_remoteSaiInterface->remove(object_type, object_id);
-//
-//    g_recorder->recordGenericRemoveResponse(status);
-//
-//    if (object_type == SAI_OBJECT_TYPE_SWITCH &&
-//            status == SAI_STATUS_SUCCESS)
-//    {
-//        SWSS_LOG_NOTICE("removing switch id %s", sai_serialize_object_id(object_id).c_str());
-//
-//        g_virtualObjectIdManager->releaseObjectId(object_id);
-//
-//        // TODO do we need some more actions here ? to clean all
-//        // objects that are in the same switch that were snooped
-//        // inside metadata ? should that be metadata job?
-//    }
-//
-//    return status;
-//}
-
 sai_status_t redis_bulk_generic_remove(
         _In_ sai_object_type_t object_type,
         _In_ uint32_t object_count,
@@ -121,23 +71,3 @@ sai_status_t internal_redis_bulk_generic_remove(
     return status;
 }
 
-
-#define REDIS_ENTRY_REMOVE(OT,ot)                           \
-    sai_status_t redis_generic_remove_ ## ot(               \
-            _In_ const sai_ ## ot ## _t *entry)             \
-        {                                                   \
-            SWSS_LOG_ENTER();                               \
-            std::string str = sai_serialize_ ## ot(*entry); \
-            return internal_redis_generic_remove(           \
-                    SAI_OBJECT_TYPE_ ## OT,                 \
-                    str);                                   \
-        }
-
-//REDIS_ENTRY_REMOVE(FDB_ENTRY,fdb_entry);
-//REDIS_ENTRY_REMOVE(INSEG_ENTRY,inseg_entry);
-//REDIS_ENTRY_REMOVE(IPMC_ENTRY,ipmc_entry);
-//REDIS_ENTRY_REMOVE(L2MC_ENTRY,l2mc_entry);
-//REDIS_ENTRY_REMOVE(MCAST_FDB_ENTRY,mcast_fdb_entry);
-//REDIS_ENTRY_REMOVE(NEIGHBOR_ENTRY,neighbor_entry);
-//REDIS_ENTRY_REMOVE(ROUTE_ENTRY,route_entry);
-//REDIS_ENTRY_REMOVE(NAT_ENTRY,nat_entry);
