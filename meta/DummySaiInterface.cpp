4566ad153f29b9ce5c2cd8a1f15ff536852463e1
kcudnik <kcudnik@gmail.com>
2019-12-31 19:55:55 +0100

    [saimeta] Add DummySaiInterface class

---
+++ meta/DummySaiInterface.cpp
@@ -0,0 +1,396 @@
+#include "DummySaiInterface.h"
+
+#include "swss/logger.h"
+
+#include <memory>
+
+using namespace saimeta;
+
+DummySaiInterface::DummySaiInterface()
+{
+    SWSS_LOG_ENTER();
+
+    m_status = SAI_STATUS_SUCCESS;
+}
+
+void DummySaiInterface::setStatus(
+        _In_ sai_status_t status)
+{
+    SWSS_LOG_ENTER();
+
+    m_status = status;
+}
+
+sai_status_t DummySaiInterface::create(
+        _In_ sai_object_type_t objectType,
+        _Out_ sai_object_id_t* objectId,
+        _In_ sai_object_id_t switchId,
+        _In_ uint32_t attr_count,
+        _In_ const sai_attribute_t *attr_list)
+{
+    SWSS_LOG_ENTER();
+
+    return m_status;
+}
+
+sai_status_t DummySaiInterface::remove(
+        _In_ sai_object_type_t objectType,
+        _In_ sai_object_id_t objectId)
+{
+    SWSS_LOG_ENTER();
+
+    return m_status;
+}
+
+sai_status_t DummySaiInterface::set(
+        _In_ sai_object_type_t objectType,
+        _In_ sai_object_id_t objectId,
+        _In_ const sai_attribute_t *attr)
+{
+    SWSS_LOG_ENTER();
+
+    return m_status;
+}
+
+sai_status_t DummySaiInterface::get(
+        _In_ sai_object_type_t objectType,
+        _In_ sai_object_id_t objectId,
+        _In_ uint32_t attr_count,
+        _Inout_ sai_attribute_t *attr_list)
+{
+    SWSS_LOG_ENTER();
+
+    return m_status;
+}
+
+#define DECLARE_REMOVE_ENTRY(OT,ot)                 \
+sai_status_t DummySaiInterface::remove(             \
+        _In_ const sai_ ## ot ## _t* ot)            \
+{                                                   \
+    SWSS_LOG_ENTER();                               \
+    return m_status;                                \
+}
+
+#define DECLARE_CREATE_ENTRY(OT,ot)                 \
+sai_status_t DummySaiInterface::create(             \
+        _In_ const sai_ ## ot ## _t* ot,            \
+        _In_ uint32_t attr_count,                   \
+        _In_ const sai_attribute_t *attr_list)      \
+{                                                   \
+    SWSS_LOG_ENTER();                               \
+    return m_status;                                \
+}
+
+#define DECLARE_SET_ENTRY(OT,ot)                    \
+sai_status_t DummySaiInterface::set(                \
+        _In_ const sai_ ## ot ## _t* ot,            \
+        _In_ const sai_attribute_t *attr)           \
+{                                                   \
+    SWSS_LOG_ENTER();                               \
+    return m_status;                                \
+}
+
+#define DECLARE_GET_ENTRY(OT,ot)                    \
+sai_status_t DummySaiInterface::get(                \
+        _In_ const sai_ ## ot ## _t* ot,            \
+        _In_ uint32_t attr_count,                   \
+        _Inout_ sai_attribute_t *attr_list)         \
+{                                                   \
+    SWSS_LOG_ENTER();                               \
+    return m_status;                                \
+}
+
+#define DECLARE_ALL(X)                  \
+    X(FDB_ENTRY,fdb_entry)              \
+    X(INSEG_ENTRY,inseg_entry);         \
+    X(IPMC_ENTRY,ipmc_entry);           \
+    X(L2MC_ENTRY,l2mc_entry);           \
+    X(MCAST_FDB_ENTRY,mcast_fdb_entry); \
+    X(NEIGHBOR_ENTRY,neighbor_entry);   \
+    X(ROUTE_ENTRY,route_entry);         \
+    X(NAT_ENTRY,nat_entry);
+
+DECLARE_ALL(DECLARE_REMOVE_ENTRY);
+DECLARE_ALL(DECLARE_CREATE_ENTRY);
+DECLARE_ALL(DECLARE_SET_ENTRY);
+DECLARE_ALL(DECLARE_GET_ENTRY);
+
+sai_status_t DummySaiInterface::flushFdbEntries(
+        _In_ sai_object_id_t switchId,
+        _In_ uint32_t attrCount,
+        _In_ const sai_attribute_t *attrList)
+{
+    SWSS_LOG_ENTER();
+
+    return m_status;
+}
+
+sai_status_t DummySaiInterface::objectTypeGetAvailability(
+        _In_ sai_object_id_t switchId,
+        _In_ sai_object_type_t objectType,
+        _In_ uint32_t attrCount,
+        _In_ const sai_attribute_t *attrList,
+        _Out_ uint64_t *count)
+{
+    SWSS_LOG_ENTER();
+
+    return m_status;
+}
+
+sai_status_t DummySaiInterface::queryAattributeEnumValuesCapability(
+        _In_ sai_object_id_t switchId,
+        _In_ sai_object_type_t objectType,
+        _In_ sai_attr_id_t attrId,
+        _Inout_ sai_s32_list_t *enumValuesCapability)
+{
+    SWSS_LOG_ENTER();
+
+    return m_status;
+}
+
+sai_status_t DummySaiInterface::getStats(
+        _In_ sai_object_type_t object_type,
+        _In_ sai_object_id_t object_id,
+        _In_ uint32_t number_of_counters,
+        _In_ const sai_stat_id_t *counter_ids,
+        _Out_ uint64_t *counters)
+{
+    SWSS_LOG_ENTER();
+
+    // TODO add recording
+
+    return m_status;
+}
+
+sai_status_t DummySaiInterface::getStatsExt(
+        _In_ sai_object_type_t object_type,
+        _In_ sai_object_id_t object_id,
+        _In_ uint32_t number_of_counters,
+        _In_ const sai_stat_id_t *counter_ids,
+        _In_ sai_stats_mode_t mode,
+        _Out_ uint64_t *counters)
+{
+    SWSS_LOG_ENTER();
+
+    return m_status;
+}
+
+sai_status_t DummySaiInterface::clearStats(
+        _In_ sai_object_type_t object_type,
+        _In_ sai_object_id_t object_id,
+        _In_ uint32_t number_of_counters,
+        _In_ const sai_stat_id_t *counter_ids)
+{
+    SWSS_LOG_ENTER();
+
+    return m_status;
+}
+
+// bulk QUAD
+
+sai_status_t DummySaiInterface::bulkRemove(
+        _In_ sai_object_type_t object_type,
+        _In_ uint32_t object_count,
+        _In_ const sai_object_id_t *object_id,
+        _In_ sai_bulk_op_error_mode_t mode,
+        _Out_ sai_status_t *object_statuses)
+{
+    SWSS_LOG_ENTER();
+
+    for (uint32_t idx = 0; idx < object_count; idx++)
+        object_statuses[idx] = m_status;
+
+    return m_status;
+}
+
+sai_status_t DummySaiInterface::bulkRemove(
+        _In_ uint32_t object_count,
+        _In_ const sai_route_entry_t *route_entry,
+        _In_ sai_bulk_op_error_mode_t mode,
+        _Out_ sai_status_t *object_statuses)
+{
+    SWSS_LOG_ENTER();
+
+    for (uint32_t idx = 0; idx < object_count; idx++)
+        object_statuses[idx] = m_status;
+
+    return m_status;
+}
+
+sai_status_t DummySaiInterface::bulkRemove(
+        _In_ uint32_t object_count,
+        _In_ const sai_nat_entry_t *nat_entry,
+        _In_ sai_bulk_op_error_mode_t mode,
+        _Out_ sai_status_t *object_statuses)
+{
+    SWSS_LOG_ENTER();
+
+    for (uint32_t idx = 0; idx < object_count; idx++)
+        object_statuses[idx] = m_status;
+
+    return m_status;
+}
+
+sai_status_t DummySaiInterface::bulkRemove(
+        _In_ uint32_t object_count,
+        _In_ const sai_fdb_entry_t *fdb_entry,
+        _In_ sai_bulk_op_error_mode_t mode,
+        _Out_ sai_status_t *object_statuses)
+{
+    SWSS_LOG_ENTER();
+
+    for (uint32_t idx = 0; idx < object_count; idx++)
+        object_statuses[idx] = m_status;
+
+    return m_status;
+}
+
+
+sai_status_t DummySaiInterface::bulkSet(
+        _In_ sai_object_type_t object_type,
+        _In_ uint32_t object_count,
+        _In_ const sai_object_id_t *object_id,
+        _In_ const sai_attribute_t *attr_list,
+        _In_ sai_bulk_op_error_mode_t mode,
+        _Out_ sai_status_t *object_statuses)
+{
+    SWSS_LOG_ENTER();
+
+    for (uint32_t idx = 0; idx < object_count; idx++)
+        object_statuses[idx] = m_status;
+
+    return m_status;
+}
+
+sai_status_t DummySaiInterface::bulkSet(
+        _In_ uint32_t object_count,
+        _In_ const sai_route_entry_t *route_entry,
+        _In_ const sai_attribute_t *attr_list,
+        _In_ sai_bulk_op_error_mode_t mode,
+        _Out_ sai_status_t *object_statuses)
+{
+    SWSS_LOG_ENTER();
+
+    for (uint32_t idx = 0; idx < object_count; idx++)
+        object_statuses[idx] = m_status;
+
+    return m_status;
+}
+
+sai_status_t DummySaiInterface::bulkSet(
+        _In_ uint32_t object_count,
+        _In_ const sai_nat_entry_t *nat_entry,
+        _In_ const sai_attribute_t *attr_list,
+        _In_ sai_bulk_op_error_mode_t mode,
+        _Out_ sai_status_t *object_statuses)
+{
+    SWSS_LOG_ENTER();
+
+    for (uint32_t idx = 0; idx < object_count; idx++)
+        object_statuses[idx] = m_status;
+
+    return m_status;
+}
+
+sai_status_t DummySaiInterface::bulkSet(
+        _In_ uint32_t object_count,
+        _In_ const sai_fdb_entry_t *fdb_entry,
+        _In_ const sai_attribute_t *attr_list,
+        _In_ sai_bulk_op_error_mode_t mode,
+        _Out_ sai_status_t *object_statuses)
+{
+    SWSS_LOG_ENTER();
+
+    for (uint32_t idx = 0; idx < object_count; idx++)
+        object_statuses[idx] = m_status;
+
+    return m_status;
+}
+
+sai_status_t DummySaiInterface::bulkCreate(
+        _In_ sai_object_type_t object_type,
+        _In_ sai_object_id_t switch_id,
+        _In_ uint32_t object_count,
+        _In_ const uint32_t *attr_count,
+        _In_ const sai_attribute_t **attr_list,
+        _In_ sai_bulk_op_error_mode_t mode,
+        _Out_ sai_object_id_t *object_id,
+        _Out_ sai_status_t *object_statuses)
+{
+    SWSS_LOG_ENTER();
+
+    for (uint32_t idx = 0; idx < object_count; idx++)
+        object_statuses[idx] = m_status;
+
+    return m_status;
+}
+
+sai_status_t DummySaiInterface::bulkCreate(
+        _In_ uint32_t object_count,
+        _In_ const sai_route_entry_t *route_entry,
+        _In_ const uint32_t *attr_count,
+        _In_ const sai_attribute_t **attr_list,
+        _In_ sai_bulk_op_error_mode_t mode,
+        _Out_ sai_status_t *object_statuses)
+{
+    SWSS_LOG_ENTER();
+
+    for (uint32_t idx = 0; idx < object_count; idx++)
+        object_statuses[idx] = m_status;
+
+    return m_status;
+}
+
+sai_status_t DummySaiInterface::bulkCreate(
+        _In_ uint32_t object_count,
+        _In_ const sai_fdb_entry_t *fdb_entry,
+        _In_ const uint32_t *attr_count,
+        _In_ const sai_attribute_t **attr_list,
+        _In_ sai_bulk_op_error_mode_t mode,
+        _Out_ sai_status_t *object_statuses)
+{
+    SWSS_LOG_ENTER();
+
+    for (uint32_t idx = 0; idx < object_count; idx++)
+        object_statuses[idx] = m_status;
+
+    return m_status;
+}
+
+sai_status_t DummySaiInterface::bulkCreate(
+        _In_ uint32_t object_count,
+        _In_ const sai_nat_entry_t *nat_entry,
+        _In_ const uint32_t *attr_count,
+        _In_ const sai_attribute_t **attr_list,
+        _In_ sai_bulk_op_error_mode_t mode,
+        _Out_ sai_status_t *object_statuses)
+{
+    SWSS_LOG_ENTER();
+
+    for (uint32_t idx = 0; idx < object_count; idx++)
+        object_statuses[idx] = m_status;
+
+    return m_status;
+}
+
+sai_object_type_t DummySaiInterface::objectTypeQuery(
+        _In_ sai_object_id_t objectId)
+{
+    SWSS_LOG_ENTER();
+
+    if (m_status != SAI_STATUS_SUCCESS)
+        SWSS_LOG_THROW("not implemented");
+
+    return SAI_OBJECT_TYPE_NULL;
+}
+
+sai_object_id_t DummySaiInterface::switchIdQuery(
+        _In_ sai_object_id_t objectId)
+{
+    SWSS_LOG_ENTER();
+
+    if (m_status != SAI_STATUS_SUCCESS)
+        SWSS_LOG_THROW("not implemented");
+
+    return SAI_NULL_OBJECT_ID;
+}
---
+++ meta/DummySaiInterface.h
@@ -0,0 +1,241 @@
+#pragma once
+
+#include "lib/inc/SaiInterface.h"
+
+#include <memory>
+
+#define SAIMETA_DUMMYSAIINTERFACE_DECLARE_REMOVE_ENTRY(ot)         \
+    virtual sai_status_t remove(                                    \
+            _In_ const sai_ ## ot ## _t* ot) override;
+
+#define SAIMETA_DUMMYSAIINTERFACE_DECLARE_CREATE_ENTRY(ot)         \
+    virtual sai_status_t create(                                    \
+            _In_ const sai_ ## ot ## _t* ot,                        \
+            _In_ uint32_t attr_count,                               \
+            _In_ const sai_attribute_t *attr_list) override;
+
+#define SAIMETA_DUMMYSAIINTERFACE_DECLARE_SET_ENTRY(ot)            \
+    virtual sai_status_t set(                                       \
+            _In_ const sai_ ## ot ## _t* ot,                        \
+            _In_ const sai_attribute_t *attr) override;
+
+#define SAIMETA_DUMMYSAIINTERFACE_DECLARE_GET_ENTRY(ot)            \
+    virtual sai_status_t get(                                       \
+            _In_ const sai_ ## ot ## _t* ot,                        \
+            _In_ uint32_t attr_count,                               \
+            _Out_ sai_attribute_t *attr_list) override;
+
+#define SAIMETA_DUMMYSAIINTERFACE_DECLARE_BULK_CREATE_ENTRY(ot)    \
+    virtual sai_status_t bulkCreate(                                \
+            _In_ uint32_t object_count,                             \
+            _In_ const sai_ ## ot ## _t *ot,                        \
+            _In_ const uint32_t *attr_count,                        \
+            _In_ const sai_attribute_t **attr_list,                 \
+            _In_ sai_bulk_op_error_mode_t mode,                     \
+            _Out_ sai_status_t *object_statuses) override;
+
+#define SAIMETA_DUMMYSAIINTERFACE_DECLARE_BULK_REMOVE_ENTRY(ot)    \
+    virtual sai_status_t bulkRemove(                                \
+            _In_ uint32_t object_count,                             \
+            _In_ const sai_ ## ot ## _t *ot,                        \
+            _In_ sai_bulk_op_error_mode_t mode,                     \
+            _Out_ sai_status_t *object_statuses) override;
+
+#define SAIMETA_DUMMYSAIINTERFACE_DECLARE_BULK_SET_ENTRY(ot)       \
+    virtual sai_status_t bulkSet(                                   \
+            _In_ uint32_t object_count,                             \
+            _In_ const sai_ ## ot ## _t *ot,                        \
+            _In_ const sai_attribute_t *attr_list,                  \
+            _In_ sai_bulk_op_error_mode_t mode,                     \
+            _Out_ sai_status_t *object_statuses) override;
+
+namespace saimeta
+{
+    /**
+     * @brief Dummy SAI interface.
+     *
+     * Defined for unittests.
+     */
+    class DummySaiInterface:
+        public sairedis::SaiInterface
+    {
+        public:
+
+            DummySaiInterface();
+
+            virtual ~DummySaiInterface() = default;
+
+        public:
+
+            void setStatus(
+                    _In_ sai_status_t status);
+
+        public: // SAI interface overrides
+
+            virtual sai_status_t create(
+                    _In_ sai_object_type_t objectType,
+                    _Out_ sai_object_id_t* objectId,
+                    _In_ sai_object_id_t switchId,
+                    _In_ uint32_t attr_count,
+                    _In_ const sai_attribute_t *attr_list) override;
+
+            virtual sai_status_t remove(
+                    _In_ sai_object_type_t objectType,
+                    _In_ sai_object_id_t objectId) override;
+
+            virtual sai_status_t set(
+                    _In_ sai_object_type_t objectType,
+                    _In_ sai_object_id_t objectId,
+                    _In_ const sai_attribute_t *attr) override;
+
+            virtual sai_status_t get(
+                    _In_ sai_object_type_t objectType,
+                    _In_ sai_object_id_t objectId,
+                    _In_ uint32_t attr_count,
+                    _Inout_ sai_attribute_t *attr_list) override;
+
+        public: // create ENTRY
+
+            SAIMETA_DUMMYSAIINTERFACE_DECLARE_CREATE_ENTRY(fdb_entry);
+            SAIMETA_DUMMYSAIINTERFACE_DECLARE_CREATE_ENTRY(inseg_entry);
+            SAIMETA_DUMMYSAIINTERFACE_DECLARE_CREATE_ENTRY(ipmc_entry);
+            SAIMETA_DUMMYSAIINTERFACE_DECLARE_CREATE_ENTRY(l2mc_entry);
+            SAIMETA_DUMMYSAIINTERFACE_DECLARE_CREATE_ENTRY(mcast_fdb_entry);
+            SAIMETA_DUMMYSAIINTERFACE_DECLARE_CREATE_ENTRY(neighbor_entry);
+            SAIMETA_DUMMYSAIINTERFACE_DECLARE_CREATE_ENTRY(route_entry);
+            SAIMETA_DUMMYSAIINTERFACE_DECLARE_CREATE_ENTRY(nat_entry);
+
+        public: // remove ENTRY
+
+            SAIMETA_DUMMYSAIINTERFACE_DECLARE_REMOVE_ENTRY(fdb_entry);
+            SAIMETA_DUMMYSAIINTERFACE_DECLARE_REMOVE_ENTRY(inseg_entry);
+            SAIMETA_DUMMYSAIINTERFACE_DECLARE_REMOVE_ENTRY(ipmc_entry);
+            SAIMETA_DUMMYSAIINTERFACE_DECLARE_REMOVE_ENTRY(l2mc_entry);
+            SAIMETA_DUMMYSAIINTERFACE_DECLARE_REMOVE_ENTRY(mcast_fdb_entry);
+            SAIMETA_DUMMYSAIINTERFACE_DECLARE_REMOVE_ENTRY(neighbor_entry);
+            SAIMETA_DUMMYSAIINTERFACE_DECLARE_REMOVE_ENTRY(route_entry);
+            SAIMETA_DUMMYSAIINTERFACE_DECLARE_REMOVE_ENTRY(nat_entry);
+
+        public: // set ENTRY
+
+            SAIMETA_DUMMYSAIINTERFACE_DECLARE_SET_ENTRY(fdb_entry);
+            SAIMETA_DUMMYSAIINTERFACE_DECLARE_SET_ENTRY(inseg_entry);
+            SAIMETA_DUMMYSAIINTERFACE_DECLARE_SET_ENTRY(ipmc_entry);
+            SAIMETA_DUMMYSAIINTERFACE_DECLARE_SET_ENTRY(l2mc_entry);
+            SAIMETA_DUMMYSAIINTERFACE_DECLARE_SET_ENTRY(mcast_fdb_entry);
+            SAIMETA_DUMMYSAIINTERFACE_DECLARE_SET_ENTRY(neighbor_entry);
+            SAIMETA_DUMMYSAIINTERFACE_DECLARE_SET_ENTRY(route_entry);
+            SAIMETA_DUMMYSAIINTERFACE_DECLARE_SET_ENTRY(nat_entry);
+
+        public: // get ENTRY
+
+            SAIMETA_DUMMYSAIINTERFACE_DECLARE_GET_ENTRY(fdb_entry);
+            SAIMETA_DUMMYSAIINTERFACE_DECLARE_GET_ENTRY(inseg_entry);
+            SAIMETA_DUMMYSAIINTERFACE_DECLARE_GET_ENTRY(ipmc_entry);
+            SAIMETA_DUMMYSAIINTERFACE_DECLARE_GET_ENTRY(l2mc_entry);
+            SAIMETA_DUMMYSAIINTERFACE_DECLARE_GET_ENTRY(mcast_fdb_entry);
+            SAIMETA_DUMMYSAIINTERFACE_DECLARE_GET_ENTRY(neighbor_entry);
+            SAIMETA_DUMMYSAIINTERFACE_DECLARE_GET_ENTRY(route_entry);
+            SAIMETA_DUMMYSAIINTERFACE_DECLARE_GET_ENTRY(nat_entry);
+
+        public: // bulk QUAD oid
+
+            virtual sai_status_t bulkCreate(
+                    _In_ sai_object_type_t object_type,
+                    _In_ sai_object_id_t switch_id,
+                    _In_ uint32_t object_count,
+                    _In_ const uint32_t *attr_count,
+                    _In_ const sai_attribute_t **attr_list,
+                    _In_ sai_bulk_op_error_mode_t mode,
+                    _Out_ sai_object_id_t *object_id,
+                    _Out_ sai_status_t *object_statuses) override;
+
+            virtual sai_status_t bulkRemove(
+                    _In_ sai_object_type_t object_type,
+                    _In_ uint32_t object_count,
+                    _In_ const sai_object_id_t *object_id,
+                    _In_ sai_bulk_op_error_mode_t mode,
+                    _Out_ sai_status_t *object_statuses) override;
+
+            virtual sai_status_t bulkSet(
+                    _In_ sai_object_type_t object_type,
+                    _In_ uint32_t object_count,
+                    _In_ const sai_object_id_t *object_id,
+                    _In_ const sai_attribute_t *attr_list,
+                    _In_ sai_bulk_op_error_mode_t mode,
+                    _Out_ sai_status_t *object_statuses) override;
+
+        public: // bulk create ENTRY
+
+            SAIMETA_DUMMYSAIINTERFACE_DECLARE_BULK_CREATE_ENTRY(fdb_entry);
+            SAIMETA_DUMMYSAIINTERFACE_DECLARE_BULK_CREATE_ENTRY(nat_entry);
+            SAIMETA_DUMMYSAIINTERFACE_DECLARE_BULK_CREATE_ENTRY(route_entry);
+
+        public: // bulk remove ENTRY
+
+            SAIMETA_DUMMYSAIINTERFACE_DECLARE_BULK_REMOVE_ENTRY(fdb_entry);
+            SAIMETA_DUMMYSAIINTERFACE_DECLARE_BULK_REMOVE_ENTRY(nat_entry);
+            SAIMETA_DUMMYSAIINTERFACE_DECLARE_BULK_REMOVE_ENTRY(route_entry);
+
+        public: // bulk set ENTRY
+
+            SAIMETA_DUMMYSAIINTERFACE_DECLARE_BULK_SET_ENTRY(fdb_entry);
+            SAIMETA_DUMMYSAIINTERFACE_DECLARE_BULK_SET_ENTRY(nat_entry);
+            SAIMETA_DUMMYSAIINTERFACE_DECLARE_BULK_SET_ENTRY(route_entry);
+
+        public: // stats API
+
+            virtual sai_status_t getStats(
+                    _In_ sai_object_type_t object_type,
+                    _In_ sai_object_id_t object_id,
+                    _In_ uint32_t number_of_counters,
+                    _In_ const sai_stat_id_t *counter_ids,
+                    _Out_ uint64_t *counters) override;
+
+            virtual sai_status_t getStatsExt(
+                    _In_ sai_object_type_t object_type,
+                    _In_ sai_object_id_t object_id,
+                    _In_ uint32_t number_of_counters,
+                    _In_ const sai_stat_id_t *counter_ids,
+                    _In_ sai_stats_mode_t mode,
+                    _Out_ uint64_t *counters) override;
+
+            virtual sai_status_t clearStats(
+                    _In_ sai_object_type_t object_type,
+                    _In_ sai_object_id_t object_id,
+                    _In_ uint32_t number_of_counters,
+                    _In_ const sai_stat_id_t *counter_ids) override;
+
+        public: // non QUAD API
+
+            virtual sai_status_t flushFdbEntries(
+                    _In_ sai_object_id_t switchId,
+                    _In_ uint32_t attrCount,
+                    _In_ const sai_attribute_t *attrList) override;
+
+        public: // SAI API
+
+            virtual sai_status_t objectTypeGetAvailability(
+                    _In_ sai_object_id_t switchId,
+                    _In_ sai_object_type_t objectType,
+                    _In_ uint32_t attrCount,
+                    _In_ const sai_attribute_t *attrList,
+                    _Out_ uint64_t *count) override;
+
+            virtual sai_status_t queryAattributeEnumValuesCapability(
+                    _In_ sai_object_id_t switch_id,
+                    _In_ sai_object_type_t object_type,
+                    _In_ sai_attr_id_t attr_id,
+                    _Inout_ sai_s32_list_t *enum_values_capability) override;
+
+            virtual sai_object_type_t objectTypeQuery(
+                    _In_ sai_object_id_t objectId) override;
+
+            virtual sai_object_id_t switchIdQuery(
+                    _In_ sai_object_id_t objectId) override;
+
+        protected:
+
+            sai_status_t m_status;
+    };
+}
