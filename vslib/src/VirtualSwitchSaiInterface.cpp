afea6a71a9f8fba41358e54d142ae941c3c0e8b9
kcudnik <kcudnik@gmail.com>
2020-01-09 18:45:32 +0100

    [vs] Move remove implementation to VirtualSwitchSaiInterface class

--- vslib/src/VirtualSwitchSaiInterface.cpp
+++ vslib/src/VirtualSwitchSaiInterface.cpp
@@ -10,6 +10,8 @@
 #include "sai_vs.h"
 #include "sai_vs_state.h"
 #include "meta/sai_serialize.h"
+#include "sai_vs_switch_BCM56850.h"
+#include "sai_vs_switch_MLNX2700.h"
 
 /*
  * Max number of counters used in 1 api call
@@ -174,13 +176,196 @@ sai_status_t VirtualSwitchSaiInterface::create(
     throw; // TODO
 }
 
+static sai_status_t internal_vs_generic_remove(
+        _In_ sai_object_type_t object_type,
+        _In_ const std::string &serialized_object_id,
+        _In_ sai_object_id_t switch_id)
+{
+    SWSS_LOG_ENTER();
+
+    auto &objectHash = g_switch_state_map.at(switch_id)->objectHash.at(object_type);
+
+    auto it = objectHash.find(serialized_object_id);
+
+    if (it == objectHash.end())
+    {
+        SWSS_LOG_ERROR("not found %s:%s",
+                sai_serialize_object_type(object_type).c_str(),
+                serialized_object_id.c_str());
+
+        return SAI_STATUS_ITEM_NOT_FOUND;
+    }
+
+    objectHash.erase(it);
+
+    return SAI_STATUS_SUCCESS;
+}
+
+static void vs_dump_switch_database_for_warm_restart(
+        _In_ sai_object_id_t switch_id)
+{
+    SWSS_LOG_ENTER();
+
+    auto it = g_switch_state_map.find(switch_id);
+
+    if (it == g_switch_state_map.end())
+    {
+        SWSS_LOG_THROW("switch don't exists 0x%" PRIx64, switch_id);
+    }
+
+    if (g_warm_boot_write_file == NULL)
+    {
+        SWSS_LOG_ERROR("warm boot write file is NULL");
+        return;
+    }
+
+    std::ofstream dumpFile;
+
+    dumpFile.open(g_warm_boot_write_file);
+
+    if (!dumpFile.is_open())
+    {
+        SWSS_LOG_ERROR("failed to open: %s", g_warm_boot_write_file);
+        return;
+    }
+
+    auto switchState = it->second;
+
+    auto objectHash = switchState->objectHash;
+
+    // dump all objects and attributes to file
+
+    size_t count = 0;
+
+    for (auto kvp: objectHash)
+    {
+        auto singleTypeObjectMap = kvp.second;
+
+        count += singleTypeObjectMap.size();
+
+        for (auto o: singleTypeObjectMap)
+        {
+            // if object don't have attributes, size can be zero
+            if (o.second.size() == 0)
+            {
+                dumpFile << sai_serialize_object_type(kvp.first) << " " << o.first << " NULL NULL" << std::endl;
+            }
+            else
+            {
+                for (auto a: o.second)
+                {
+                    dumpFile << sai_serialize_object_type(kvp.first) << " ";
+                    dumpFile << o.first.c_str();
+                    dumpFile << " ";
+                    dumpFile << a.first.c_str();
+                    dumpFile << " ";
+                    dumpFile << a.second->getAttrStrValue();
+                    dumpFile << std::endl;
+                }
+            }
+        }
+    }
+
+    if (g_vs_hostif_use_tap_device)
+    {
+        /*
+         * If user is using tap devices we also need to dump local fdb info
+         * data and restore it on warm start.
+         */
+
+        for (auto fi: g_fdb_info_set)
+        {
+            dumpFile << SAI_VS_FDB_INFO << " " << sai_vs_serialize_fdb_info(fi) << std::endl;
+        }
+
+        SWSS_LOG_NOTICE("dumped %zu fdb infos", g_fdb_info_set.size());
+    }
+
+    dumpFile.close();
+
+    SWSS_LOG_NOTICE("dumped %zu objects to %s", count, g_warm_boot_write_file);
+}
+
 sai_status_t VirtualSwitchSaiInterface::remove(
         _In_ sai_object_type_t objectType,
         _In_ const std::string& serializedObjectId)
 {
     SWSS_LOG_ENTER();
 
-    throw; // TODO
+    // std::string str_object_id = sai_serialize_object_id(object_id);
+    // sai_object_id_t switch_id = g_realObjectIdManager->saiSwitchIdQuery(object_id);
+
+    sai_object_id_t switch_id;
+
+    if (g_switch_state_map.size() == 1)
+    {
+        switch_id = g_switch_state_map.begin()->first;
+    }
+    else
+    {
+        SWSS_LOG_THROW("multiple switches not supported, FIXME");
+    }
+
+    /*
+     * Perform db dump if warm restart was requested.
+     */
+
+    if (objectType == SAI_OBJECT_TYPE_SWITCH)
+    {
+        sai_attribute_t attr;
+
+        attr.id = SAI_SWITCH_ATTR_RESTART_WARM;
+
+        sai_object_id_t object_id;
+        sai_deserialize_object_id(serializedObjectId, object_id);
+
+        if (vs_generic_get(objectType, object_id, 1, &attr) == SAI_STATUS_SUCCESS)
+        {
+            SWSS_LOG_NOTICE("SAI_SWITCH_ATTR_RESTART_WARM = %s", attr.value.booldata ? "true" : "false");
+
+            if (attr.value.booldata)
+            {
+                vs_dump_switch_database_for_warm_restart(object_id);
+            }
+        }
+        else
+        {
+            SWSS_LOG_ERROR("failed to get SAI_SWITCH_ATTR_RESTART_WARM, no DB dump will be performed");
+        }
+    }
+
+    sai_status_t status = internal_vs_generic_remove(
+            objectType,
+            serializedObjectId,
+            switch_id);
+
+    if (objectType == SAI_OBJECT_TYPE_SWITCH &&
+            status == SAI_STATUS_SUCCESS)
+    {
+        sai_object_id_t object_id;
+        sai_deserialize_object_id(serializedObjectId, object_id);
+
+        SWSS_LOG_NOTICE("removed switch: %s", sai_serialize_object_id(object_id).c_str());
+
+        g_realObjectIdManager->releaseObjectId(object_id);
+
+        switch (g_vs_switch_type)
+        {
+            case SAI_VS_SWITCH_TYPE_BCM56850:
+                uninit_switch_BCM56850(object_id);
+                break;
+
+            case SAI_VS_SWITCH_TYPE_MLNX2700:
+                uninit_switch_MLNX2700(object_id);
+                break;
+
+            default:
+                SWSS_LOG_WARN("unknown switch type: %d", g_vs_switch_type);
+                break;
+        }
+    }
+
+    return status;
 }
 
 static sai_status_t internal_vs_generic_set(
--- vslib/src/sai_vs_generic_remove.cpp
+++ vslib/src/sai_vs_generic_remove.cpp
@@ -1,120 +1,6 @@
 #include "sai_vs.h"
 #include "sai_vs_state.h"
-#include "sai_vs_switch_BCM56850.h"
-#include "sai_vs_switch_MLNX2700.h"
 
-#include <inttypes.h>
-#include <fstream>
-
-sai_status_t internal_vs_generic_remove(
-        _In_ sai_object_type_t object_type,
-        _In_ const std::string &serialized_object_id,
-        _In_ sai_object_id_t switch_id)
-{
-    SWSS_LOG_ENTER();
-
-    auto &objectHash = g_switch_state_map.at(switch_id)->objectHash.at(object_type);
-
-    auto it = objectHash.find(serialized_object_id);
-
-    if (it == objectHash.end())
-    {
-        SWSS_LOG_ERROR("not found %s:%s",
-                sai_serialize_object_type(object_type).c_str(),
-                serialized_object_id.c_str());
-
-        return SAI_STATUS_ITEM_NOT_FOUND;
-    }
-
-    objectHash.erase(it);
-
-    return SAI_STATUS_SUCCESS;
-}
-
-void vs_dump_switch_database_for_warm_restart(
-        _In_ sai_object_id_t switch_id)
-{
-    SWSS_LOG_ENTER();
-
-    auto it = g_switch_state_map.find(switch_id);
-
-    if (it == g_switch_state_map.end())
-    {
-        SWSS_LOG_THROW("switch don't exists 0x%" PRIx64, switch_id);
-    }
-
-    if (g_warm_boot_write_file == NULL)
-    {
-        SWSS_LOG_ERROR("warm boot write file is NULL");
-        return;
-    }
-
-    std::ofstream dumpFile;
-
-    dumpFile.open(g_warm_boot_write_file);
-
-    if (!dumpFile.is_open())
-    {
-        SWSS_LOG_ERROR("failed to open: %s", g_warm_boot_write_file);
-        return;
-    }
-
-    auto switchState = it->second;
-
-    auto objectHash = switchState->objectHash;
-
-    // dump all objects and attributes to file
-
-    size_t count = 0;
-
-    for (auto kvp: objectHash)
-    {
-        auto singleTypeObjectMap = kvp.second;
-
-        count += singleTypeObjectMap.size();
-
-        for (auto o: singleTypeObjectMap)
-        {
-            // if object don't have attributes, size can be zero
-            if (o.second.size() == 0)
-            {
-                dumpFile << sai_serialize_object_type(kvp.first) << " " << o.first << " NULL NULL" << std::endl;
-            }
-            else
-            {
-                for (auto a: o.second)
-                {
-                    dumpFile << sai_serialize_object_type(kvp.first) << " ";
-                    dumpFile << o.first.c_str();
-                    dumpFile << " ";
-                    dumpFile << a.first.c_str();
-                    dumpFile << " ";
-                    dumpFile << a.second->getAttrStrValue();
-                    dumpFile << std::endl;
-                }
-            }
-        }
-    }
-
-    if (g_vs_hostif_use_tap_device)
-    {
-        /*
-         * If user is using tap devices we also need to dump local fdb info
-         * data and restore it on warm start.
-         */
-
-        for (auto fi: g_fdb_info_set)
-        {
-            dumpFile << SAI_VS_FDB_INFO << " " << sai_vs_serialize_fdb_info(fi) << std::endl;
-        }
-
-        SWSS_LOG_NOTICE("dumped %zu fdb infos", g_fdb_info_set.size());
-    }
-
-    dumpFile.close();
-
-    SWSS_LOG_NOTICE("dumped %zu objects to %s", count, g_warm_boot_write_file);
-}
 
 sai_status_t vs_generic_remove(
         _In_ sai_object_type_t object_type,
@@ -122,64 +8,7 @@ sai_status_t vs_generic_remove(
 {
     SWSS_LOG_ENTER();
 
-    std::string str_object_id = sai_serialize_object_id(object_id);
-
-    sai_object_id_t switch_id = g_realObjectIdManager->saiSwitchIdQuery(object_id);
-
-    /*
-     * Perform db dump if warm restart was requested.
-     */
-
-    if (object_type == SAI_OBJECT_TYPE_SWITCH)
-    {
-        sai_attribute_t attr;
-
-        attr.id = SAI_SWITCH_ATTR_RESTART_WARM;
-
-        if (vs_generic_get(object_type, object_id, 1, &attr) == SAI_STATUS_SUCCESS)
-        {
-            SWSS_LOG_NOTICE("SAI_SWITCH_ATTR_RESTART_WARM = %s", attr.value.booldata ? "true" : "false");
-
-            if (attr.value.booldata)
-            {
-                vs_dump_switch_database_for_warm_restart(object_id);
-            }
-        }
-        else
-        {
-            SWSS_LOG_ERROR("failed to get SAI_SWITCH_ATTR_RESTART_WARM, no DB dump will be performed");
-        }
-    }
-
-    sai_status_t status = internal_vs_generic_remove(
-            object_type,
-            str_object_id,
-            switch_id);
-
-    if (object_type == SAI_OBJECT_TYPE_SWITCH &&
-            status == SAI_STATUS_SUCCESS)
-    {
-        SWSS_LOG_NOTICE("removed switch: %s", sai_serialize_object_id(object_id).c_str());
-
-        g_realObjectIdManager->releaseObjectId(object_id);
-
-        switch (g_vs_switch_type)
-        {
-            case SAI_VS_SWITCH_TYPE_BCM56850:
-                uninit_switch_BCM56850(object_id);
-                break;
-
-            case SAI_VS_SWITCH_TYPE_MLNX2700:
-                uninit_switch_MLNX2700(object_id);
-                break;
-
-            default:
-                SWSS_LOG_WARN("unknown switch type: %d", g_vs_switch_type);
-                break;
-        }
-    }
-
-    return status;
+    return g_vs->remove(object_type, object_id);
 }
 
 #define VS_ENTRY_REMOVE(OT,ot)                              \
@@ -187,11 +16,7 @@ sai_status_t vs_generic_remove(
             _In_ const sai_ ## ot ## _t *entry)             \
         {                                                   \
             SWSS_LOG_ENTER();                               \
-            std::string str = sai_serialize_ ## ot(*entry); \
-            return internal_vs_generic_remove(              \
-                    SAI_OBJECT_TYPE_ ## OT,                 \
-                    str,                                    \
-                    entry->switch_id);                      \
+            return g_vs->remove(entry);                     \
         }
 
 VS_ENTRY_REMOVE(FDB_ENTRY,fdb_entry);
