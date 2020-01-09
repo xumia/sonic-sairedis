fc8be7824994af15e6985fb7a36f21d0f76a91db
kcudnik <kcudnik@gmail.com>
2020-01-09 21:58:33 +0100

    [vs] Add SwitchState class

---
+++ vslib/inc/SwitchState.h
@@ -0,0 +1,97 @@
+#pragma once
+
+extern "C" {
+#include "sai.h"
+}
+
+#include "SaiAttrWrap.h"
+#include "swss/selectableevent.h"
+
+#include <map>
+#include <memory>
+#include <thread>
+#include <string>
+
+namespace saivs
+{
+    class SwitchState
+    {
+        public:
+
+            /**
+             * @brief AttrHash key is attribute ID, value is actual attribute
+             */
+            typedef std::map<std::string, std::shared_ptr<SaiAttrWrap>> AttrHash;
+
+            /**
+             * @brief ObjectHash is map indexed by object type and then serialized object id.
+             */
+            typedef std::map<sai_object_type_t, std::map<std::string, AttrHash>> ObjectHash;
+
+            typedef std::map<sai_object_id_t, std::shared_ptr<SwitchState>> SwitchStateMap;
+
+        public:
+
+            SwitchState(
+                    _In_ sai_object_id_t switch_id);
+
+            virtual ~SwitchState() = default;
+
+        public: // TODO make private
+
+            ObjectHash m_objectHash;
+
+            std::map<std::string, std::map<int, uint64_t>> m_countersMap;
+
+        public:
+
+            sai_object_id_t getSwitchId() const;
+
+            bool getRunLinkThread() const;
+
+            void setRunLinkThread(
+                    _In_ bool run);
+
+            swss::SelectableEvent* getLinkThreadEvent();
+
+            void setLinkThread(
+                    _In_ std::shared_ptr<std::thread> thread);
+
+            std::shared_ptr<std::thread> getLinkThread() const;
+
+            void setIfNameToPortId(
+                    _In_ const std::string& ifname,
+                    _In_ sai_object_id_t port_id);
+
+            void removeIfNameToPortId(
+                    _In_ const std::string& ifname);
+
+            sai_object_id_t getPortIdFromIfName(
+                    _In_ const std::string& ifname) const;
+
+            void setPortIdToTapName(
+                    _In_ sai_object_id_t port_id,
+                    _In_ const std::string& tapname);
+
+            void removePortIdToTapName(
+                    _In_ sai_object_id_t port_id);
+
+            bool  getTapNameFromPortId(
+                    _In_ const sai_object_id_t port_id,
+                    _Out_ std::string& if_name);
+
+        private:
+
+            sai_object_id_t m_switch_id;
+
+            std::map<sai_object_id_t, std::string> m_port_id_to_tapname;
+
+            swss::SelectableEvent m_link_thread_event;
+
+            volatile bool m_run_link_thread;
+
+            std::shared_ptr<std::thread> m_link_thread;
+
+            std::map<std::string, sai_object_id_t> m_ifname_to_port_id_map;
+    };
+}
---
+++ vslib/src/SwitchState.cpp
@@ -0,0 +1,151 @@
+#include "SwitchState.h"
+#include "RealObjectIdManager.h"
+
+#include "swss/logger.h"
+
+#include "meta/sai_serialize.h"
+
+using namespace saivs;
+
+extern std::shared_ptr<RealObjectIdManager>       g_realObjectIdManager;
+
+SwitchState::SwitchState(
+        _In_ sai_object_id_t switch_id):
+    m_switch_id(switch_id)
+{
+    SWSS_LOG_ENTER();
+
+    if (g_realObjectIdManager->saiObjectTypeQuery(switch_id) != SAI_OBJECT_TYPE_SWITCH)
+    {
+        SWSS_LOG_THROW("object %s is not SWITCH, its %s",
+                sai_serialize_object_id(switch_id).c_str(),
+                sai_serialize_object_type(g_realObjectIdManager->saiObjectTypeQuery(switch_id)).c_str());
+    }
+
+    for (int i = SAI_OBJECT_TYPE_NULL; i < (int)SAI_OBJECT_TYPE_EXTENSIONS_MAX; ++i)
+    {
+        /*
+         * Populate empty maps for each object to avoid checking if
+         * objecttype exists.
+         */
+
+        m_objectHash[(sai_object_type_t)i] = { };
+    }
+
+    /*
+     * Create switch by default, it will require special treat on
+     * creating.
+     */
+
+    m_objectHash[SAI_OBJECT_TYPE_SWITCH][sai_serialize_object_id(switch_id)] = {};
+}
+
+sai_object_id_t SwitchState::getSwitchId() const
+{
+    SWSS_LOG_ENTER();
+
+    return m_switch_id;
+}
+
+bool SwitchState::getRunLinkThread() const
+{
+    SWSS_LOG_ENTER();
+
+    return m_run_link_thread;
+}
+
+void SwitchState::setRunLinkThread(
+        _In_ bool run)
+{
+    SWSS_LOG_ENTER();
+
+    m_run_link_thread = run;
+}
+
+swss::SelectableEvent* SwitchState::getLinkThreadEvent()
+{
+    SWSS_LOG_ENTER();
+
+    return &m_link_thread_event;
+}
+
+void SwitchState::setLinkThread(
+        _In_ std::shared_ptr<std::thread> thread)
+{
+    SWSS_LOG_ENTER();
+
+    m_link_thread = thread;
+}
+
+std::shared_ptr<std::thread> SwitchState::getLinkThread() const
+{
+    SWSS_LOG_ENTER();
+
+    return m_link_thread;
+}
+
+void SwitchState::setIfNameToPortId(
+        _In_ const std::string& ifname,
+        _In_ sai_object_id_t port_id)
+{
+    SWSS_LOG_ENTER();
+
+    m_ifname_to_port_id_map[ifname] = port_id;
+}
+
+
+void SwitchState::removeIfNameToPortId(
+        _In_ const std::string& ifname)
+{
+    SWSS_LOG_ENTER();
+
+    m_ifname_to_port_id_map.erase(ifname);
+}
+
+sai_object_id_t SwitchState::getPortIdFromIfName(
+        _In_ const std::string& ifname) const
+{
+    SWSS_LOG_ENTER();
+
+    auto it = m_ifname_to_port_id_map.find(ifname);
+
+    if (it == m_ifname_to_port_id_map.end())
+    {
+        return SAI_NULL_OBJECT_ID;
+    }
+
+    return it->second;
+}
+
+void SwitchState::setPortIdToTapName(
+        _In_ sai_object_id_t port_id,
+        _In_ const std::string& tapname)
+{
+    SWSS_LOG_ENTER();
+
+    m_port_id_to_tapname[port_id] = tapname;
+}
+
+void SwitchState::removePortIdToTapName(
+        _In_ sai_object_id_t port_id)
+{
+    SWSS_LOG_ENTER();
+
+    m_port_id_to_tapname.erase(port_id);
+}
+
+bool SwitchState::getTapNameFromPortId(
+        _In_ const sai_object_id_t port_id,
+        _Out_ std::string& if_name)
+{
+    SWSS_LOG_ENTER();
+
+    if (m_port_id_to_tapname.find(port_id) != m_port_id_to_tapname.end())
+    {
+        if_name = m_port_id_to_tapname[port_id];
+
+        return true;
+    }
+
+    return false;
+}
