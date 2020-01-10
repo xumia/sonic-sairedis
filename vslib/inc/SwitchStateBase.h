efde37d3f1ee8bb68d5426a9024a09a17a526898
kcudnik <kcudnik@gmail.com>
2020-01-10 14:18:42 +0100

    [vs] Add SwitchStateBase class

---
+++ vslib/inc/SwitchStateBase.h
@@ -0,0 +1,52 @@
+
+#pragma once
+
+#include "SwitchState.h"
+
+namespace saivs
+{
+    class SwitchStateBase:
+        public SwitchState
+    {
+        public:
+
+            SwitchStateBase(
+                    _In_ sai_object_id_t switch_id);
+
+            virtual ~SwitchStateBase() = default;
+
+        public: // TODO to protected
+
+            virtual sai_status_t set_switch_mac_address();
+
+            virtual sai_status_t set_switch_default_attributes();
+
+            virtual sai_status_t create_default_vlan();
+
+        protected:
+
+            virtual sai_status_t create(
+                    _In_ sai_object_type_t object_type,
+                    _Out_ sai_object_id_t *object_id,
+                    _In_ sai_object_id_t switch_id,
+                    _In_ uint32_t attr_count,
+                    _In_ const sai_attribute_t *attr_list);
+
+            virtual sai_status_t set(
+                    _In_ sai_object_type_t objectType,
+                    _In_ sai_object_id_t objectId,
+                    _In_ const sai_attribute_t* attr);
+
+        public: // TODO to proctected
+
+            std::vector<sai_object_id_t> m_port_list;
+            std::vector<sai_object_id_t> m_bridge_port_list_port_based;
+
+            std::vector<sai_acl_action_type_t> ingress_acl_action_list;
+            std::vector<sai_acl_action_type_t> m_egress_acl_action_list;
+
+            sai_object_id_t m_default_vlan_id;
+
+    };
+}
+
---
+++ vslib/src/SwitchStateBase.cpp
@@ -0,0 +1,114 @@
+#include "SwitchStateBase.h"
+
+#include "swss/logger.h"
+
+#include "sai_vs.h" // TODO to be removed
+
+using namespace saivs;
+
+SwitchStateBase::SwitchStateBase(
+                    _In_ sai_object_id_t switch_id):
+    SwitchState(switch_id)
+{
+    SWSS_LOG_ENTER();
+
+    // empty
+}
+
+sai_status_t SwitchStateBase::create(
+        _In_ sai_object_type_t object_type,
+        _Out_ sai_object_id_t *object_id,
+        _In_ sai_object_id_t switch_id,
+        _In_ uint32_t attr_count,
+        _In_ const sai_attribute_t *attr_list)
+{
+    SWSS_LOG_ENTER();
+
+    return vs_generic_create(object_type, object_id, switch_id, attr_count, attr_list);
+}
+
+sai_status_t SwitchStateBase::set(
+        _In_ sai_object_type_t objectType,
+        _In_ sai_object_id_t objectId,
+        _In_ const sai_attribute_t* attr)
+{
+    SWSS_LOG_ENTER();
+
+    return vs_generic_set(objectType, objectId, attr);
+}
+
+sai_status_t SwitchStateBase::set_switch_mac_address()
+{
+    SWSS_LOG_ENTER();
+
+    SWSS_LOG_INFO("create switch src mac address");
+
+    sai_attribute_t attr;
+
+    attr.id = SAI_SWITCH_ATTR_SRC_MAC_ADDRESS;
+
+    attr.value.mac[0] = 0x11;
+    attr.value.mac[1] = 0x22;
+    attr.value.mac[2] = 0x33;
+    attr.value.mac[3] = 0x44;
+    attr.value.mac[4] = 0x55;
+    attr.value.mac[5] = 0x66;
+
+    return set(SAI_OBJECT_TYPE_SWITCH, m_switch_id, &attr);
+}
+
+sai_status_t SwitchStateBase::set_switch_default_attributes()
+{
+    SWSS_LOG_ENTER();
+
+    SWSS_LOG_INFO("create switch default attributes");
+
+    sai_attribute_t attr;
+
+    attr.id = SAI_SWITCH_ATTR_PORT_STATE_CHANGE_NOTIFY;
+    attr.value.ptr = NULL;
+
+    CHECK_STATUS(set(SAI_OBJECT_TYPE_SWITCH, m_switch_id, &attr));
+
+    attr.id = SAI_SWITCH_ATTR_FDB_EVENT_NOTIFY;
+
+    CHECK_STATUS(set(SAI_OBJECT_TYPE_SWITCH, m_switch_id, &attr));
+
+    attr.id = SAI_SWITCH_ATTR_FDB_AGING_TIME;
+    attr.value.u32 = 0;
+
+    CHECK_STATUS(set(SAI_OBJECT_TYPE_SWITCH, m_switch_id, &attr));
+
+    attr.id = SAI_SWITCH_ATTR_RESTART_WARM;
+    attr.value.booldata = false;
+
+    CHECK_STATUS(set(SAI_OBJECT_TYPE_SWITCH, m_switch_id, &attr));
+
+    attr.id = SAI_SWITCH_ATTR_WARM_RECOVER;
+    attr.value.booldata = false;
+
+    return set(SAI_OBJECT_TYPE_SWITCH, m_switch_id, &attr);
+}
+
+sai_status_t SwitchStateBase::create_default_vlan()
+{
+    SWSS_LOG_ENTER();
+
+    SWSS_LOG_INFO("create default vlan");
+
+    sai_attribute_t attr;
+
+    attr.id = SAI_VLAN_ATTR_VLAN_ID;
+    attr.value.u16 = DEFAULT_VLAN_NUMBER;
+
+    CHECK_STATUS(create(SAI_OBJECT_TYPE_VLAN, &m_default_vlan_id, m_switch_id, 1, &attr));
+
+    /* set default vlan on switch */
+
+    attr.id = SAI_SWITCH_ATTR_DEFAULT_VLAN_ID;
+    attr.value.oid = m_default_vlan_id;
+
+    return set(SAI_OBJECT_TYPE_SWITCH, m_switch_id, &attr);
+}
+
+
