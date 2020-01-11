16311a220f1d87988791b65cb43f41c3d7214f56
kcudnik <kcudnik@gmail.com>
2020-01-11 14:47:52 +0100

    [vs] Start using LinkMsg class

--- vslib/src/sai_vs_switch.cpp
+++ vslib/src/sai_vs_switch.cpp
@@ -7,9 +7,7 @@
 #include "swss/select.h"
 #include "swss/netdispatcher.h"
 
-#include <netlink/route/link.h>
-#include <netlink/route/addr.h>
-#include <linux/if.h>
+#include "LinkMsg.h"
 
 #include <thread>
 
@@ -64,127 +62,6 @@ void update_port_oper_status(
                 sai_serialize_port_oper_status(port_oper_status).c_str());
     }
 }
-
-class LinkMsg : public swss::NetMsg
-{
-    public:
-
-        LinkMsg(sai_object_id_t switch_id)
-            : m_switch_id(switch_id)
-        {
-            SWSS_LOG_ENTER();
-        }
-
-        virtual void onMsg(int nlmsg_type, struct nl_object *obj)
-        {
-            SWSS_LOG_ENTER();
-
-            if (nlmsg_type == RTM_DELLINK)
-            {
-                struct rtnl_link *link = (struct rtnl_link *)obj;
-                const char* name = rtnl_link_get_name(link);
-
-                SWSS_LOG_NOTICE("received RTM_DELLINK for %s", name);
-                return;
-            }
-
-            if (nlmsg_type != RTM_NEWLINK)
-            {
-                SWSS_LOG_WARN("unsupported nlmsg_type: %d", nlmsg_type);
-                return;
-            }
-
-            // new link
-
-            struct rtnl_link *link = (struct rtnl_link *)obj;
-
-            int             if_index = rtnl_link_get_ifindex(link);
-            unsigned int    if_flags = rtnl_link_get_flags(link); // IFF_LOWER_UP and IFF_RUNNING
-            const char*     if_name  = rtnl_link_get_name(link);
-
-            if (strncmp(if_name, SAI_VS_VETH_PREFIX, sizeof(SAI_VS_VETH_PREFIX) - 1) != 0 &&
-                    g_ifname_to_lanes.find(if_name) == g_ifname_to_lanes.end())
-            {
-                SWSS_LOG_INFO("skipping newlink for %s", if_name);
-                return;
-            }
-
-            SWSS_LOG_NOTICE("newlink: ifindex: %d, ifflags: 0x%x, ifname: %s",
-                    if_index,
-                    if_flags,
-                    if_name);
-
-            std::string ifname(if_name);
-
-            std::shared_ptr<SwitchState> sw = vs_get_switch_state(m_switch_id);
-
-            if (sw == nullptr)
-            {
-                SWSS_LOG_ERROR("failed to get switch state for switch id %s",
-                        sai_serialize_object_id(m_switch_id).c_str());
-                return;
-            }
-
-            auto port_id = sw->getPortIdFromIfName(ifname); // TODO needs to be protected under lock
-
-            if (port_id == SAI_NULL_OBJECT_ID)
-            {
-                SWSS_LOG_ERROR("failed to find port id for interface %s", ifname.c_str());
-                return;
-            }
-
-            sai_attribute_t attr;
-
-            attr.id = SAI_SWITCH_ATTR_PORT_STATE_CHANGE_NOTIFY;
-
-            if (vs_switch_api.get_switch_attribute(m_switch_id, 1, &attr) != SAI_STATUS_SUCCESS)
-            {
-                SWSS_LOG_ERROR("failed to get SAI_SWITCH_ATTR_PORT_STATE_CHANGE_NOTIFY for switch %s",
-                        sai_serialize_object_id(m_switch_id).c_str());
-                return;
-            }
-
-            sai_port_state_change_notification_fn callback =
-                (sai_port_state_change_notification_fn)attr.value.ptr;
-
-            if (callback == NULL)
-            {
-                return;
-            }
-
-            sai_port_oper_status_notification_t data;
-
-            data.port_id = port_id;
-            data.port_state = (if_flags & IFF_LOWER_UP) ? SAI_PORT_OPER_STATUS_UP : SAI_PORT_OPER_STATUS_DOWN;
-
-            attr.id = SAI_PORT_ATTR_OPER_STATUS;
-
-            if (vs_port_api.get_port_attribute(port_id, 1, &attr) != SAI_STATUS_SUCCESS)
-            {
-                SWSS_LOG_ERROR("failed to get port attribute SAI_PORT_ATTR_OPER_STATUS");
-            }
-            else
-            {
-                if ((sai_port_oper_status_t)attr.value.s32 == data.port_state)
-                {
-                    SWSS_LOG_DEBUG("port oper status didn't changed, will not send notification");
-                    return;
-                }
-            }
-
-            update_port_oper_status(port_id, data.port_state);
-
-            SWSS_LOG_DEBUG("executing callback SAI_SWITCH_ATTR_PORT_STATE_CHANGE_NOTIFY for port %s: %s",
-                    sai_serialize_object_id(data.port_id).c_str(),
-                    sai_serialize_port_oper_status(data.port_state).c_str());
-
-            callback(1, &data);
-        }
-
-    private:
-
-        sai_object_id_t m_switch_id;
-};
 
 void linkmsg_thread(
         _In_ sai_object_id_t switch_id)
