b5a778fa05d68c9a859134e18d9cb712be9bfcf8
kcudnik <kcudnik@gmail.com>
2020-01-12 17:34:58 +0100

    [vs] Add class LaneMapContainer

---
+++ vslib/inc/LaneMapContainer.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "LaneMap.h"
+
+#include <memory>
+#include <map>
+
+namespace saivs
+{
+    class LaneMapContainer
+    {
+        public:
+
+            LaneMapContainer() = default;
+
+            virtual ~LaneMapContainer() = default;
+
+        public:
+
+            void insert(
+                    _In_ std::shared_ptr<LaneMap> laneMap);
+
+            void remove(
+                    _In_ uint32_t switchIndex);
+
+            std::shared_ptr<LaneMap> getLaneMap(
+                    _In_ uint32_t switchIndex) const;
+
+            void clear();
+
+            bool hasLaneMap(
+                    _In_ uint32_t switchIndex) const;
+
+            size_t size() const;
+
+            void removeEmptyLaneMaps();
+
+        private:
+
+            std::map<uint32_t, std::shared_ptr<LaneMap>> m_map;
+    };
+}
+
---
+++ vslib/src/LaneMapContainer.cpp
@@ -0,0 +1,80 @@
+#include "LaneMapContainer.h"
+
+#include "swss/logger.h"
+
+using namespace saivs;
+
+void LaneMapContainer::insert(
+        _In_ std::shared_ptr<LaneMap> laneMap)
+{
+    SWSS_LOG_ENTER();
+
+    m_map[laneMap->getSwitchIndex()] = laneMap;
+}
+
+void LaneMapContainer::remove(
+        _In_ uint32_t switchIndex)
+{
+    SWSS_LOG_ENTER();
+
+    auto it = m_map.find(switchIndex);
+
+    if (it != m_map.end())
+    {
+        m_map.erase(it);
+    }
+}
+
+std::shared_ptr<LaneMap> LaneMapContainer::getLaneMap(
+        _In_ uint32_t switchIndex) const
+{
+    SWSS_LOG_ENTER();
+
+    auto it = m_map.find(switchIndex);
+
+    if (it == m_map.end())
+    {
+        return nullptr;
+    }
+
+    return it->second;
+}
+
+void LaneMapContainer::clear()
+{
+    SWSS_LOG_ENTER();
+
+    m_map.clear();
+}
+
+bool LaneMapContainer::hasLaneMap(
+        _In_ uint32_t switchIndex) const
+{
+    SWSS_LOG_ENTER();
+
+    return m_map.find(switchIndex) != m_map.end();
+}
+
+size_t LaneMapContainer::size() const
+{
+    SWSS_LOG_ENTER();
+
+    return m_map.size();
+}
+
+void LaneMapContainer::removeEmptyLaneMaps()
+{
+    SWSS_LOG_ENTER();
+
+    for(auto it = m_map.begin(); it != m_map.end();)
+    {
+        if (it->second->isEmpty())
+        {
+            it = m_map.erase(it);
+        }
+        else
+        {
+            ++it;
+        }
+    }
+}
