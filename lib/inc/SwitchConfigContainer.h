d06751c6000c63de2e14527eacec7a7ebf11c699
kcudnik <kcudnik@gmail.com>
2020-01-22 19:57:35 +0100

    [sairedis] Add SwitchConfigContainer class

---
+++ lib/inc/SwitchConfigContainer.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "SwitchConfig.h"
+
+#include "swss/sal.h"
+
+#include <map>
+#include <memory>
+
+namespace sairedis
+{
+    class SwitchConfigContainer
+    {
+        public:
+
+            SwitchConfigContainer() = default;
+
+            virtual ~SwitchConfigContainer() = default;
+
+        public:
+
+            void insert(
+                    _In_ std::shared_ptr<SwitchConfig> config);
+
+            std::shared_ptr<SwitchConfig> getConfig(
+                    _In_ uint32_t switchIndex) const;
+
+            std::shared_ptr<SwitchConfig> getConfig(
+                    _In_ const std::string& hardwareInfo) const;
+
+        private:
+
+            std::map<uint32_t, std::shared_ptr<SwitchConfig>> m_indexToConfig;
+
+            std::map<std::string, std::shared_ptr<SwitchConfig>> m_hwinfoToConfig;
+    };
+}
---
+++ lib/src/SwitchConfigContainer.cpp
@@ -0,0 +1,64 @@
+#include "SwitchConfigContainer.h"
+
+#include "swss/logger.h"
+
+using namespace sairedis;
+
+void SwitchConfigContainer::insert(
+        _In_ std::shared_ptr<SwitchConfig> config)
+{
+    SWSS_LOG_ENTER();
+
+    auto it = m_indexToConfig.find(config->m_switchIndex);
+
+    if (it != m_indexToConfig.end())
+    {
+        SWSS_LOG_THROW("switch with index %u already exists", config->m_switchIndex);
+    }
+
+    auto itt = m_hwinfoToConfig.find(config->m_hardwareInfo);
+
+    if (itt != m_hwinfoToConfig.end())
+    {
+        SWSS_LOG_THROW("switch with hwinfo '%s' already exists",
+                config->m_hardwareInfo.c_str());
+    }
+
+    SWSS_LOG_NOTICE("added switch: %u:%s",
+            config->m_switchIndex,
+            config->m_hardwareInfo.c_str());
+
+    m_indexToConfig[config->m_switchIndex] = config;
+
+    m_hwinfoToConfig[config->m_hardwareInfo] = config;
+}
+
+std::shared_ptr<SwitchConfig> SwitchConfigContainer::getConfig(
+        _In_ uint32_t switchIndex) const
+{
+    SWSS_LOG_ENTER();
+
+    auto it = m_indexToConfig.find(switchIndex);
+
+    if (it != m_indexToConfig.end())
+    {
+        return it->second;
+    }
+
+    return nullptr;
+}
+
+std::shared_ptr<SwitchConfig> SwitchConfigContainer::getConfig(
+        _In_ const std::string& hardwareInfo) const
+{
+    SWSS_LOG_ENTER();
+
+    auto it = m_hwinfoToConfig.find(hardwareInfo);
+
+    if (it != m_hwinfoToConfig.end())
+    {
+        return it->second;
+    }
+
+    return nullptr;
+}
