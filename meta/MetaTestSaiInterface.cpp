d4d531066ebb29714e3239327cbc702f08b59fb1
kcudnik <kcudnik@gmail.com>
2020-01-22 20:17:35 +0100

    [meta] Fix tests

--- meta/MetaTestSaiInterface.cpp
+++ meta/MetaTestSaiInterface.cpp
@@ -1,5 +1,6 @@
 #include "MetaTestSaiInterface.h"
 #include "NumberOidIndexGenerator.h"
+#include "SwitchConfigContainer.h"
 
 #include "swss/logger.h"
 #include "sai_serialize.h"
@@ -10,9 +11,60 @@ MetaTestSaiInterface::MetaTestSaiInterface()
 {
     SWSS_LOG_ENTER();
 
+    auto sc = std::make_shared<sairedis::SwitchConfig>();
+
+    sc->m_switchIndex = 0;
+    sc->m_hardwareInfo = "";
+
+    auto scc = std::make_shared<sairedis::SwitchConfigContainer>();
+
+    scc->insert(sc);
+
     m_virtualObjectIdManager = 
-        std::make_shared<sairedis::VirtualObjectIdManager>(0,
+        std::make_shared<sairedis::VirtualObjectIdManager>(0, scc,
                 std::make_shared<NumberOidIndexGenerator>());
+}
+
+static std::string getHardwareInfo(
+        _In_ uint32_t attrCount,
+        _In_ const sai_attribute_t *attrList)
+{
+    SWSS_LOG_ENTER();
+
+     auto *attr = sai_metadata_get_attr_by_id(
+             SAI_SWITCH_ATTR_SWITCH_HARDWARE_INFO,
+             attrCount,
+             attrList);
+
+     if (attr == NULL)
+         return "";
+
+     auto& s8list = attr->value.s8list;
+
+     if (s8list.count == 0)
+         return "";
+
+     if (s8list.list == NULL)
+     {
+         SWSS_LOG_WARN("SAI_SWITCH_ATTR_SWITCH_HARDWARE_INFO s8list.list is NULL! but count is %u", s8list.count);
+         return "";
+     }
+
+     uint32_t count = s8list.count;
+
+     if (count > SAI_MAX_HARDWARE_ID_LEN)
+     {
+         SWSS_LOG_WARN("SAI_SWITCH_ATTR_SWITCH_HARDWARE_INFO s8list.count (%u) > SAI_MAX_HARDWARE_ID_LEN (%d), LIMITING !!",
+                 count,
+                 SAI_MAX_HARDWARE_ID_LEN);
+
+         count = SAI_MAX_HARDWARE_ID_LEN;
+     }
+
+     // check actual length, since buffer may contain nulls
+     auto actualLength = strnlen((const char*)s8list.list, count);
+
+     return std::string((const char*)s8list.list, actualLength);
 }
 
 sai_status_t MetaTestSaiInterface::create(
@@ -24,7 +76,28 @@ sai_status_t MetaTestSaiInterface::create(
 {
     SWSS_LOG_ENTER();
 
-    *objectId = m_virtualObjectIdManager->allocateNewObjectId(objectType, switchId);
+    if (objectType == SAI_OBJECT_TYPE_SWITCH)
+    {
+        // for given hardware info we always return same switch id,
+        // this is required since we could be performing warm boot here
+
+        auto hwinfo = getHardwareInfo(attr_count, attr_list);
+
+        switchId = m_virtualObjectIdManager->allocateNewSwitchObjectId(hwinfo);
+
+        *objectId = switchId;
+
+        if (switchId == SAI_NULL_OBJECT_ID)
+        {
+            SWSS_LOG_ERROR("switch ID allocation failed");
+
+            return SAI_STATUS_FAILURE;
+        }
+    }
+    else
+    {
+        *objectId = m_virtualObjectIdManager->allocateNewObjectId(objectType, switchId);
+    }
 
     if (*objectId == SAI_NULL_OBJECT_ID)
     {
