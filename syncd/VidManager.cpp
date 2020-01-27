c43581ed6f75e65abf6630b71284cd3bc2feaaeb
kcudnik <kcudnik@gmail.com>
2020-01-27 14:25:04 +0100

    [syncd] Add VidManager class

---
+++ syncd/VidManager.cpp
@@ -0,0 +1,78 @@
+#include "VidManager.h"
+
+#include "lib/inc/VirtualObjectIdManager.h"
+
+#include "meta/sai_serialize.h"
+
+#include "swss/logger.h"
+
+using namespace syncd;
+
+sai_object_id_t VidManager::switchIdQuery(
+        _In_ sai_object_id_t objectId)
+{
+    SWSS_LOG_ENTER();
+
+    if (objectId == SAI_NULL_OBJECT_ID)
+    {
+        return objectId;
+    }
+
+    auto swid = sairedis::VirtualObjectIdManager::switchIdQuery(objectId);
+
+    if (swid == SAI_NULL_OBJECT_ID)
+    {
+        SWSS_LOG_THROW("invalid object id %s",
+                sai_serialize_object_id(objectId).c_str());
+    }
+
+    return swid;
+}
+
+sai_object_type_t VidManager::objectTypeQuery(
+        _In_ sai_object_id_t objectId)
+{
+    SWSS_LOG_ENTER();
+
+    if (objectId == SAI_NULL_OBJECT_ID)
+    {
+        return SAI_OBJECT_TYPE_NULL;
+    }
+
+    sai_object_type_t ot = sairedis::VirtualObjectIdManager::objectTypeQuery(objectId);
+
+    if (ot == SAI_OBJECT_TYPE_NULL)
+    {
+        SWSS_LOG_THROW("invalid object id %s",
+                sai_serialize_object_id(objectId).c_str());
+    }
+
+    return ot;
+}
+
+uint32_t VidManager::getSwitchIndex(
+        _In_ sai_object_id_t objectId)
+{
+    SWSS_LOG_ENTER();
+
+    sai_object_id_t swid = VidManager::switchIdQuery(objectId);
+
+    if (swid != SAI_NULL_OBJECT_ID)
+    {
+        return sairedis::VirtualObjectIdManager::getSwitchIndex(swid);
+    }
+
+    SWSS_LOG_THROW("invalid obejct id: %s, should be SWITCH",
+            sai_serialize_object_id(objectId).c_str());
+
+}
+
+uint32_t VidManager::getGlobalContext(
+        _In_ sai_object_id_t obejctId)
+{
+    SWSS_LOG_ENTER();
+
+    SWSS_LOG_ERROR("not implemented, FIXME");
+
+    return 0; // TODO implement
+}
---
+++ syncd/VidManager.h
@@ -0,0 +1,74 @@
+#pragma once
+
+extern "C" {
+#include "sai.h"
+}
+
+namespace syncd
+{
+    class VidManager
+    {
+
+        private:
+
+            VidManager() = delete;
+
+            ~VidManager() = delete;
+
+        public:
+
+            /**
+             * @brief Switch id query.
+             *
+             * Return switch object id for given object if. If object type is
+             * switch, it will return input value.
+             *
+             * For SAI_NULL_OBJECT_ID returns SAI_NULL_OBJECT_ID.
+             *
+             * Throws for invalid object ID.
+             */
+            static sai_object_id_t switchIdQuery(
+                    _In_ sai_object_id_t objectId);
+
+            /**
+             * @brief Object type query.
+             *
+             * Returns object type for input object id. If object id is invalid
+             * then returns SAI_OBJECT_TYPE_NULL.
+             *
+             * For SAI_NULL_OBJECT_ID returns SAI_OBJECT_TYPE_NULL.
+             *
+             * Throws for invalid object ID.
+             */
+            static sai_object_type_t objectTypeQuery(
+                    _In_ sai_object_id_t objectId);
+
+            /**
+             * @brief Get switch index.
+             *
+             * Index range is <0..255>.
+             *
+             * Returns switch index for given oid. 
+             *
+             * For SAI_NULL_OBJECT_ID returns 0.
+             *
+             * Throws for invalid object ID.
+             */
+            static uint32_t getSwitchIndex(
+                    _In_ sai_object_id_t obejctId);
+
+            /**
+             * @brief Get global context ID.
+             *
+             * Context range is <0..255>.
+             *
+             * Returns switch index for given oid. 
+             *
+             * For SAI_NULL_OBJECT_ID returns 0.
+             *
+             * Throws for invalid object ID.
+             */
+            static uint32_t getGlobalContext(
+                    _In_ sai_object_id_t obejctId);
+    };
+}
