b7437b4adf2128df4569c269df76a24f78e9af0c
kcudnik <kcudnik@gmail.com>
2020-01-30 14:25:55 +0100

    [syncd] Add SaiDiscovery class

---
+++ syncd/SaiDiscovery.cpp
@@ -0,0 +1,319 @@
+#include "SaiDiscovery.h"
+
+#include "swss/logger.h"
+
+#include "meta/sai_serialize.h"
+
+using namespace syncd;
+
+/**
+ * @def SAI_DISCOVERY_LIST_MAX_ELEMENTS
+ *
+ * Defines maximum elements that can be obtained from the OID list when
+ * performing list attribute query (discovery) on the switch.
+ *
+ * This value will be used to allocate memory on the stack for obtaining object
+ * list, and should be big enough to obtain list for all ports on the switch
+ * and vlan members.
+ */
+#define SAI_DISCOVERY_LIST_MAX_ELEMENTS 1024
+
+
+SaiDiscovery::SaiDiscovery(
+        _In_ std::shared_ptr<sairedis::SaiInterface> sai):
+    m_sai(sai)
+{
+    SWSS_LOG_ENTER();
+
+    // empty
+}
+
+void SaiDiscovery::discover(
+        _In_ sai_object_id_t rid,
+        _Inout_ std::set<sai_object_id_t> &discovered)
+{
+    SWSS_LOG_ENTER();
+
+    /*
+     * NOTE: This method is only good after switch init since we are making
+     * assumptions that there are no ACL after initialization.
+     *
+     * NOTE: Input set could be a map of sets, this way we will also have
+     * dependency on each oid.
+     */
+
+    if (rid == SAI_NULL_OBJECT_ID)
+    {
+        return;
+    }
+
+    if (discovered.find(rid) != discovered.end())
+    {
+        return;
+    }
+
+    sai_object_type_t ot = m_sai->objectTypeQuery(rid);
+
+    if (ot == SAI_OBJECT_TYPE_NULL)
+    {
+        SWSS_LOG_THROW("objectTypeQuery: rid %s returned NULL object type",
+                sai_serialize_object_id(rid).c_str());
+    }
+
+    SWSS_LOG_DEBUG("processing %s: %s",
+            sai_serialize_object_id(rid).c_str(),
+            sai_serialize_object_type(ot).c_str());
+
+    /*
+     * We will ignore STP ports by now, since when removing bridge port, then
+     * associated stp port is automatically removed, and we don't use STP in
+     * out solution.  This causing inconsistency with redis ASIC view vs
+     * actual ASIC asic state.
+     *
+     * TODO: This needs to be solved by sending discovered state to sairedis
+     * metadata db for reference count.
+     *
+     * XXX: workaround
+     */
+
+    if (ot != SAI_OBJECT_TYPE_STP_PORT)
+    {
+        discovered.insert(rid);
+    }
+
+    const sai_object_type_info_t *info =  sai_metadata_get_object_type_info(ot);
+
+    /*
+     * We will query only oid object types
+     * then we don't need meta key, but we need to add to metadata
+     * pointers to only generic functions.
+     */
+
+    sai_object_meta_key_t mk = { .objecttype = ot, .objectkey = { .key = { .object_id = rid } } };
+
+    for (int idx = 0; info->attrmetadata[idx] != NULL; ++idx)
+    {
+        const sai_attr_metadata_t *md = info->attrmetadata[idx];
+
+        /*
+         * Note that we don't care about ACL object id's since
+         * we assume that there are no ACLs on switch after init.
+         */
+
+        sai_attribute_t attr;
+
+        attr.id = md->attrid;
+
+        if (md->attrvaluetype == SAI_ATTR_VALUE_TYPE_OBJECT_ID)
+        {
+            if (md->defaultvaluetype == SAI_DEFAULT_VALUE_TYPE_CONST)
+            {
+                /*
+                 * This means that default value for this object is
+                 * SAI_NULL_OBJECT_ID, since this is discovery after
+                 * create, we don't need to query this attribute.
+                 */
+
+                //continue;
+            }
+
+            if (md->objecttype == SAI_OBJECT_TYPE_STP &&
+                    md->attrid == SAI_STP_ATTR_BRIDGE_ID)
+            {
+                // XXX workaround (for mlnx)
+                SWSS_LOG_WARN("skipping since it causes crash: %s", md->attridname);
+                continue;
+            }
+
+            if (md->objecttype == SAI_OBJECT_TYPE_BRIDGE_PORT)
+            {
+                if (md->attrid == SAI_BRIDGE_PORT_ATTR_TUNNEL_ID ||
+                        md->attrid == SAI_BRIDGE_PORT_ATTR_RIF_ID)
+                {
+                    /*
+                     * We know that bridge port is bound on PORT, no need
+                     * to query those attributes.
+                     */
+
+                    continue;
+                }
+            }
+
+            SWSS_LOG_DEBUG("getting %s for %s", md->attridname,
+                    sai_serialize_object_id(rid).c_str());
+
+            sai_status_t status = info->get(&mk, 1, &attr);
+
+            if (status != SAI_STATUS_SUCCESS)
+            {
+                /*
+                 * We failed to get value, maybe it's not supported ?
+                 */
+
+                SWSS_LOG_INFO("%s: %s on %s",
+                        md->attridname,
+                        sai_serialize_status(status).c_str(),
+                        sai_serialize_object_id(rid).c_str());
+
+                continue;
+            }
+
+            m_defaultOidMap[rid][attr.id] = attr.value.oid;
+
+            if (!md->allownullobjectid && attr.value.oid == SAI_NULL_OBJECT_ID)
+            {
+                // SWSS_LOG_WARN("got null on %s, but not allowed", md->attridname);
+            }
+
+            if (attr.value.oid != SAI_NULL_OBJECT_ID)
+            {
+                ot = m_sai->objectTypeQuery(attr.value.oid);
+
+                if (ot == SAI_OBJECT_TYPE_NULL)
+                {
+                    SWSS_LOG_THROW("when query %s (on %s RID %s) got value %s objectTypeQuery returned NULL object type",
+                            md->attridname,
+                            sai_serialize_object_type(md->objecttype).c_str(),
+                            sai_serialize_object_id(rid).c_str(),
+                            sai_serialize_object_id(attr.value.oid).c_str());
+                }
+            }
+
+            discover(attr.value.oid, discovered); // recursion
+        }
+        else if (md->attrvaluetype == SAI_ATTR_VALUE_TYPE_OBJECT_LIST)
+        {
+            if (md->defaultvaluetype == SAI_DEFAULT_VALUE_TYPE_EMPTY_LIST)
+            {
+                /*
+                 * This means that default value for this object is
+                 * empty list, since this is discovery after
+                 * create, we don't need to query this attribute.
+                 */
+
+                //continue;
+            }
+
+            SWSS_LOG_DEBUG("getting %s for %s", md->attridname,
+                    sai_serialize_object_id(rid).c_str());
+
+            sai_object_id_t local[SAI_DISCOVERY_LIST_MAX_ELEMENTS];
+
+            attr.value.objlist.count = SAI_DISCOVERY_LIST_MAX_ELEMENTS;
+            attr.value.objlist.list = local;
+
+            sai_status_t status = info->get(&mk, 1, &attr);
+
+            if (status != SAI_STATUS_SUCCESS)
+            {
+                /*
+                 * We failed to get value, maybe it's not supported ?
+                 */
+
+                SWSS_LOG_INFO("%s: %s on %s",
+                        md->attridname,
+                        sai_serialize_status(status).c_str(),
+                        sai_serialize_object_id(rid).c_str());
+
+                continue;
+            }
+
+            SWSS_LOG_DEBUG("list count %s %u", md->attridname, attr.value.objlist.count);
+
+            for (uint32_t i = 0; i < attr.value.objlist.count; ++i)
+            {
+                sai_object_id_t oid = attr.value.objlist.list[i];
+
+                ot = m_sai->objectTypeQuery(oid);
+
+                if (ot == SAI_OBJECT_TYPE_NULL)
+                {
+                    SWSS_LOG_THROW("when query %s (on %s RID %s) got value %s objectTypeQuery returned NULL object type",
+                            md->attridname,
+                            sai_serialize_object_type(md->objecttype).c_str(),
+                            sai_serialize_object_id(rid).c_str(),
+                            sai_serialize_object_id(oid).c_str());
+                }
+
+                discover(oid, discovered); // recursion
+            }
+        }
+    }
+}
+
+std::set<sai_object_id_t> SaiDiscovery::discover(
+        _In_ sai_object_id_t startRid)
+{
+    SWSS_LOG_ENTER();
+
+    /*
+     * Preform discovery on the switch to obtain ASIC view of
+     * objects that are created internally.
+     */
+
+    m_defaultOidMap.clear();
+
+    std::set<sai_object_id_t> discovered_rids;
+
+    {
+        SWSS_LOG_TIMER("discover");
+
+        setApiLogLevel(SAI_LOG_LEVEL_CRITICAL);
+
+        discover(startRid, discovered_rids);
+
+        setApiLogLevel(SAI_LOG_LEVEL_NOTICE);
+    }
+
+    SWSS_LOG_NOTICE("discovered objects count: %zu", discovered_rids.size());
+
+    std::map<sai_object_type_t, int> map;
+
+    for (sai_object_id_t rid: discovered_rids)
+    {
+        /*
+         * We don't need to check for null since saiDiscovery already checked
+         * that.
+         */
+
+        map[m_sai->objectTypeQuery(rid)]++;
+    }
+
+    for (const auto &p: map)
+    {
+        SWSS_LOG_NOTICE("%s: %d", sai_serialize_object_type(p.first).c_str(), p.second);
+    }
+
+    return discovered_rids;
+}
+
+const SaiDiscovery::DefaultOidMap& SaiDiscovery::getDefaultOidMap() const
+{
+    SWSS_LOG_ENTER();
+
+    return m_defaultOidMap;
+}
+
+void SaiDiscovery::setApiLogLevel(
+        _In_ sai_log_level_t logLevel)
+{
+    SWSS_LOG_ENTER();
+
+    // We start from 1 since 0 is SAI_API_UNSPECIFIED.
+
+    for (uint32_t api = 1; api < sai_metadata_enum_sai_api_t.valuescount; ++api)
+    {
+        sai_status_t status = m_sai->logSet((sai_api_t)api, logLevel);
+
+        if (status == SAI_STATUS_SUCCESS)
+        {
+            SWSS_LOG_NOTICE("Setting SAI loglevel %s on %s",
+                    sai_serialize_log_level(logLevel).c_str(),
+                    sai_serialize_api((sai_api_t)api).c_str());
+        }
+        else
+        {
+            SWSS_LOG_INFO("set loglevel failed: %s", sai_serialize_status(status).c_str());
+        }
+    }
+}
---
+++ syncd/SaiDiscovery.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include "lib/inc/SaiInterface.h"
+
+#include <memory>
+#include <set>
+#include <unordered_map>
+
+namespace syncd
+{
+    class SaiDiscovery
+    {
+        public:
+
+            typedef std::unordered_map<sai_object_id_t, std::unordered_map<sai_attr_id_t, sai_object_id_t>> DefaultOidMap;
+
+        public:
+
+            SaiDiscovery(
+                    _In_ std::shared_ptr<sairedis::SaiInterface> sai);
+
+            virtual ~SaiDiscovery() = default;
+
+        public:
+
+            std::set<sai_object_id_t> discover(
+                    _In_ sai_object_id_t rid);
+
+            const DefaultOidMap& getDefaultOidMap() const;
+
+        private:
+
+            /**
+             * @brief Discover objects on the switch.
+             *
+             * Method will query recursively all OID attributes (oid and list) on
+             * the given object.
+             *
+             * This method should be called only once inside constructor right
+             * after switch has been created to obtain actual ASIC view.
+             *
+             * @param rid Object to discover other objects.
+             * @param processed Set of already processed objects. This set will be
+             * updated every time new object ID is discovered.
+             */
+            void discover(
+                    _In_ sai_object_id_t rid,
+                    _Inout_ std::set<sai_object_id_t> &processed);
+
+            void setApiLogLevel(
+                    _In_ sai_log_level_t logLevel);
+
+        private:
+
+            std::shared_ptr<sairedis::SaiInterface> m_sai;
+
+            DefaultOidMap m_defaultOidMap;
+    };
+}
