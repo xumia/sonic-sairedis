37fc317302fe355d775d5aed5b7693aa4e334618
kcudnik <kcudnik@gmail.com>
2020-02-23 19:46:12 +0100

    [sairedis] Fix SwitchConfig constructor

--- lib/src/SwitchConfig.cpp
+++ lib/src/SwitchConfig.cpp
@@ -16,7 +16,7 @@ SwitchConfig::SwitchConfig():
 SwitchConfig::SwitchConfig(
         _In_ uint32_t switchIndex,
         _In_ const std::string& hwinfo):
-    m_switchIndex(0),
+    m_switchIndex(switchIndex),
     m_hardwareInfo(hwinfo)
 {
     SWSS_LOG_ENTER();
