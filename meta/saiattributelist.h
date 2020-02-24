1809f7aaef1770bd44fab6bcaf26e812cdfbf667
kcudnik <kcudnik@gmail.com>
2020-02-24 15:24:55 +0100

    [meta] Add missing includes for saiattributelist.h

--- meta/saiattributelist.h
+++ meta/saiattributelist.h
@@ -2,6 +2,12 @@
 
 #include "SaiAttributeList.h"
 
+#include <hiredis/hiredis.h>
+#include "swss/dbconnector.h"
+#include "swss/logger.h"
+#include "sai_serialize.h"
+#include "string.h"
+
 using namespace saimeta;
 
 // NOTE: this header is for backward compatibility swss
