036ffbaaba3ca4d0d698430484b3c3485a67abbc
vganesan-nokia <67648637+vganesan-nokia@users.noreply.github.com>
2020-11-10 03:30:17 -0500

    [vs] SAI support for VOQ switches - Core Port Index Map File parser (#700)
    Signed-off-by: vedganes <vedavinayagam.ganesan@nokia.com>
    
    Core port index map file parser class. The Core port index map is used
    for voq switch testing in vs target

---
+++ vslib/inc/CorePortIndexMapFileParser.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "CorePortIndexMapContainer.h"
+
+#include <vector>
+#include <memory>
+
+namespace saivs
+{
+    class CorePortIndexMapFileParser
+    {
+        private:
+
+            CorePortIndexMapFileParser() = delete;
+            ~CorePortIndexMapFileParser() = delete;
+
+        public:
+
+            static std::shared_ptr<CorePortIndexMapContainer> parseCorePortIndexMapFile(
+                    _In_ const char* file);
+
+            static std::shared_ptr<CorePortIndexMapContainer> parseCorePortIndexMapFile(
+                    _In_ const std::string& file);
+
+            static bool isInterfaceNameValid(
+                    _In_ const std::string& name);
+
+        private:
+
+            static void parseLineWithIndex(
+                    _In_ std::shared_ptr<CorePortIndexMapContainer> container,
+                    _In_ const std::vector<std::string>& tokens);
+
+            static void parseLineWithNoIndex(
+                    _In_ std::shared_ptr<CorePortIndexMapContainer> container,
+                    _In_ const std::vector<std::string>& tokens);
+
+            static void parse(
+                    _In_ std::shared_ptr<CorePortIndexMapContainer> container,
+                    _In_ uint32_t switchIndex,
+                    _In_ const std::string& ifname,
+                    _In_ const std::string& slanes);
+
+    };
+}
