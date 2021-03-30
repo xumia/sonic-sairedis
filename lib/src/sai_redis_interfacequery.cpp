00d883cafbf9c5834017155ef0973ae5e2a0ea98
Kamil Cudnik <kcudnik@gmail.com>
2021-03-30 11:35:21 +0200

    [sairedis] Add sai_dbg_generate_dump api to redis interface (#811)

--- lib/src/sai_redis_interfacequery.cpp
+++ lib/src/sai_redis_interfacequery.cpp
@@ -167,3 +167,11 @@ sai_object_id_t sai_switch_id_query(
 
     return redis_sai->switchIdQuery(objectId);
 }
+
+sai_status_t sai_dbg_generate_dump(
+        _In_ const char *dump_file_name)
+{
+    SWSS_LOG_ENTER();
+
+    return SAI_STATUS_NOT_IMPLEMENTED;
+}
