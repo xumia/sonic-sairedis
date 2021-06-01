0e2105a8e5c6e14c3551ec2bf65b1ad41bb5060b
SuvarnaMeenakshi <50386592+SuvarnaMeenakshi@users.noreply.github.com>
2021-06-01 09:40:19 -0700

    [vs]: Start syncd by passing context configuration file and global context index. (#832)
    In multi-asic VS switch, there are multiple swss and syncd dockers. Orchagent process in each swss is started with a different hwinfo(asic_id). This PR is to ensure that context configuration is passed in syncd which has the hwinfo information with which each orchagent is started.

--- syncd/scripts/syncd_init_common.sh
+++ syncd/scripts/syncd_init_common.sh
@@ -250,6 +250,9 @@ config_syncd_nephos()
 config_syncd_vs()
 {
     CMD_ARGS+=" -p $HWSKU_DIR/sai.profile"
+    if [ -f $HWSKU_DIR/context_config.json ]; then
+        CMD_ARGS+=" -x $HWSKU_DIR/context_config.json -g 0"
+    fi
 }
 
 config_syncd_innovium()
