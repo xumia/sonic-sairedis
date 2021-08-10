f1b632c46ed40778256bade0462ac5767ceb2819
kcudnik <kcudnik@gmail.com>
2021-08-10 14:19:15 +0200

    Update autoclean.sh
    Use git to clean ignored files

--- autoclean.sh
+++ autoclean.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-rm -rf aclocal.m4 autom4te.cache stamp-h1 libtool configure config.* Makefile.in Makefile config/* m4/*
+git clean -dfX
 
-find -type f -name "*~" -print0 | xargs -0 rm -f
-find -type f -name "*.sw[po]" -print0 | xargs -0 rm -f
-find -type f -name "Makefile" -print0 | xargs -0 rm -f
-find -type f -name "Makefile.in" -print0 | xargs -0 rm -f
+(cd SAI && git clean -dfX)
 
-find -type d -name .deps -print0 | xargs -0 rm -rf
-
+true
