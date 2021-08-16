d3419e0c89af1ccbbf42cc869ade7a1e40a6c600
kcudnik <kcudnik@gmail.com>
2021-08-16 18:36:56 +0200

    Add acl tests

---
+++ unittest/vslib/test_sai_vs_acl.cpp
@@ -0,0 +1,48 @@
+#include <gtest/gtest.h>
+
+extern "C" {
+#include "sai.h"
+}
+
+#include "swss/logger.h"
+
+TEST(libsaivs, acl)
+{
+    sai_acl_api_t *api = nullptr;
+
+    sai_api_query(SAI_API_ACL, (void**)&api);
+
+    EXPECT_NE(api, nullptr);
+
+    sai_object_id_t id;
+
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->create_acl_table(&id,0,0,0));
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->remove_acl_table(0));
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->set_acl_table_attribute(0,0));
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->get_acl_table_attribute(0,0,0));
+
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->create_acl_entry(&id,0,0,0));
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->remove_acl_entry(0));
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->set_acl_entry_attribute(0,0));
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->get_acl_entry_attribute(0,0,0));
+
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->create_acl_counter(&id,0,0,0));
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->remove_acl_counter(0));
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->set_acl_counter_attribute(0,0));
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->get_acl_counter_attribute(0,0,0));
+
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->create_acl_range(&id,0,0,0));
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->remove_acl_range(0));
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->set_acl_range_attribute(0,0));
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->get_acl_range_attribute(0,0,0));
+
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->create_acl_table_group(&id,0,0,0));
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->remove_acl_table_group(0));
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->set_acl_table_group_attribute(0,0));
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->get_acl_table_group_attribute(0,0,0));
+
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->create_acl_table_group_member(&id,0,0,0));
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->remove_acl_table_group_member(0));
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->set_acl_table_group_member_attribute(0,0));
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->get_acl_table_group_member_attribute(0,0,0));
+}
