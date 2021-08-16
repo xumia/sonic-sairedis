2200e38fb7064edbb0aee500841747298d788aeb
kcudnik <kcudnik@gmail.com>
2021-08-16 20:39:56 +0200

    Add next hop tests

---
+++ unittest/vslib/test_sai_vs_nexthop.cpp
@@ -0,0 +1,23 @@
+#include <gtest/gtest.h>
+
+extern "C" {
+#include "sai.h"
+}
+
+#include "swss/logger.h"
+
+TEST(libsaivs, next_hop)
+{
+    sai_next_hop_api_t *api = nullptr;
+
+    sai_api_query(SAI_API_NEXT_HOP, (void**)&api);
+
+    EXPECT_NE(api, nullptr);
+
+    sai_object_id_t id;
+
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->create_next_hop(&id,0,0,0));
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->remove_next_hop(0));
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->set_next_hop_attribute(0,0));
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->get_next_hop_attribute(0,0,0));
+}
