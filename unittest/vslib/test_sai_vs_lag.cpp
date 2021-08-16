27a2b90bd5090ec3afdbed6e768428c00ac4c22c
kcudnik <kcudnik@gmail.com>
2021-08-16 20:36:15 +0200

    Add neighbor tests

--- unittest/vslib/test_sai_vs_lag.cpp
+++ unittest/vslib/test_sai_vs_lag.cpp
@@ -6,26 +6,30 @@ extern "C" {
 
 #include "swss/logger.h"
 
-TEST(libsaivs, lag)
+TEST(libsaivs, vlan)
 {
-    sai_lag_api_t *api = nullptr;
+    sai_vlan_api_t *api = nullptr;
 
-    sai_api_query(SAI_API_LAG, (void**)&api);
+    sai_api_query(SAI_API_VLAN, (void**)&api);
 
     EXPECT_NE(api, nullptr);
 
     sai_object_id_t id;
 
-    EXPECT_NE(SAI_STATUS_SUCCESS, api->create_lag(&id,0,0,0));
-    EXPECT_NE(SAI_STATUS_SUCCESS, api->remove_lag(0));
-    EXPECT_NE(SAI_STATUS_SUCCESS, api->set_lag_attribute(0,0));
-    EXPECT_NE(SAI_STATUS_SUCCESS, api->get_lag_attribute(0,0,0));
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->create_vlan(&id,0,0,0));
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->remove_vlan(0));
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->set_vlan_attribute(0,0));
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->get_vlan_attribute(0,0,0));
 
-    EXPECT_NE(SAI_STATUS_SUCCESS, api->create_lag_member(&id,0,0,0));
-    EXPECT_NE(SAI_STATUS_SUCCESS, api->remove_lag_member(0));
-    EXPECT_NE(SAI_STATUS_SUCCESS, api->set_lag_member_attribute(0,0));
-    EXPECT_NE(SAI_STATUS_SUCCESS, api->get_lag_member_attribute(0,0,0));
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->create_vlan_member(&id,0,0,0));
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->remove_vlan_member(0));
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->set_vlan_member_attribute(0,0));
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->get_vlan_member_attribute(0,0,0));
 
-    EXPECT_NE(SAI_STATUS_SUCCESS, api->create_lag_members(0,0,0,0,SAI_BULK_OP_ERROR_MODE_IGNORE_ERROR,0,0));
-    EXPECT_NE(SAI_STATUS_SUCCESS, api->remove_lag_members(0,0,SAI_BULK_OP_ERROR_MODE_IGNORE_ERROR,0));
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->create_vlan_members(0,0,0,0,SAI_BULK_OP_ERROR_MODE_IGNORE_ERROR,0,0));
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->remove_vlan_members(0,0,SAI_BULK_OP_ERROR_MODE_IGNORE_ERROR,0));
+
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->get_vlan_stats(0,0,0,0));
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->get_vlan_stats_ext(0,0,0,SAI_STATS_MODE_READ,0));
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->clear_vlan_stats(0,0,0));
 }
