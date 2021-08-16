f6d5d7d34c442a5e09ca04d332ac9734eb703ee1
kcudnik <kcudnik@gmail.com>
2021-08-16 18:52:49 +0200

    Add bfd tests

---
+++ unittest/vslib/test_sai_vs_bfd.cpp
@@ -0,0 +1,27 @@
+#include <gtest/gtest.h>
+
+extern "C" {
+#include "sai.h"
+}
+
+#include "swss/logger.h"
+
+TEST(libsaivs, bfd)
+{
+    sai_bfd_api_t *api = nullptr;
+
+    sai_api_query(SAI_API_BFD, (void**)&api);
+
+    EXPECT_NE(api, nullptr);
+
+    sai_object_id_t id;
+
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->create_bfd_session(&id,0,0,0));
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->remove_bfd_session(0));
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->set_bfd_session_attribute(0,0));
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->get_bfd_session_attribute(0,0,0));
+
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->get_bfd_session_stats(0,0,0,0));
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->get_bfd_session_stats_ext(0,0,0,SAI_STATS_MODE_READ,0));
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->clear_bfd_session_stats(0,0,0));
+}
