27bbde704f77d6e479719ddf1c3aa93ba189a608
kcudnik <kcudnik@gmail.com>
2021-08-17 12:15:36 +0200

    Add udf tests

---
+++ unittest/vslib/test_sai_vs_udf.cpp
@@ -0,0 +1,33 @@
+#include <gtest/gtest.h>
+
+extern "C" {
+#include "sai.h"
+}
+
+#include "swss/logger.h"
+
+TEST(libsaivs, udf)
+{
+    sai_udf_api_t *api = nullptr;
+
+    sai_api_query(SAI_API_UDF, (void**)&api);
+
+    EXPECT_NE(api, nullptr);
+
+    sai_object_id_t id;
+
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->create_udf(&id,0,0,0));
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->remove_udf(0));
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->set_udf_attribute(0,0));
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->get_udf_attribute(0,0,0));
+
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->create_udf_match(&id,0,0,0));
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->remove_udf_match(0));
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->set_udf_match_attribute(0,0));
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->get_udf_match_attribute(0,0,0));
+
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->create_udf_group(&id,0,0,0));
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->remove_udf_group(0));
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->set_udf_group_attribute(0,0));
+    EXPECT_NE(SAI_STATUS_SUCCESS, api->get_udf_group_attribute(0,0,0));
+}
