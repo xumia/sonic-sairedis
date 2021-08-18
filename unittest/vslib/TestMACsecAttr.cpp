8ed6e365a83767c8a7e74dc57aeae528c8e65e30
kcudnik <kcudnik@gmail.com>
2021-08-18 19:15:47 +0200

    Add MACsecAttr tests

---
+++ unittest/vslib/TestMACsecAttr.cpp
@@ -0,0 +1,15 @@
+#include "MACsecAttr.h"
+
+#include <gtest/gtest.h>
+
+using namespace saivs;
+
+TEST(MACsecAttr, ctr)
+{
+    MACsecAttr sec;
+}
+
+TEST(MACsecAttr, dtr)
+{
+    MACsecAttr sec;
+}
