7dbcc5bb9988cbefc92b39cb16b953d2aade4572
kcudnik <kcudnik@gmail.com>
2021-08-24 20:56:57 +0200

    Move serialization meta/tests to gtest

--- unittest/meta/TestSaiSerialize.cpp
+++ unittest/meta/TestSaiSerialize.cpp
@@ -1,11 +1,29 @@
 #include "sai_serialize.h"
+#include "MetaTestSaiInterface.h"
+#include "Meta.h"
 
 #include "sairedis.h"
 #include "sairediscommon.h"
 
+#include <inttypes.h>
+#include <arpa/inet.h>
+
 #include <gtest/gtest.h>
 
 #include <memory>
+
+using namespace saimeta;
+
+//static std::shared_ptr<MetaTestSaiInterface> g_sai = std::make_shared<MetaTestSaiInterface>();
+//static std::shared_ptr<Meta> g_meta = std::make_shared<Meta>(g_sai);
+//
+//static void clear_local()
+//{
+//    SWSS_LOG_ENTER();
+//
+//    g_sai = std::make_shared<MetaTestSaiInterface>();
+//    g_meta = std::make_shared<Meta>(g_sai);
+//}
 
 TEST(SaiSerialize, transfer_attributes)
 {
@@ -487,4 +505,665 @@ TEST(SaiSerialize, sai_deserialize)
     sai_deserialize("SYNCD_INSPECT_ASIC", value);
 
     EXPECT_EQ(value, SAI_REDIS_NOTIFY_SYNCD_INSPECT_ASIC);
+}
+
+TEST(SaiSerialize, serialize_bool)
+{
+    //clear_local();
+
+    sai_attribute_t attr;
+    const sai_attr_metadata_t* meta;
+    std::string s;
+
+    // test bool
+
+    attr.id = SAI_SWITCH_ATTR_ON_LINK_ROUTE_SUPPORTED;
+    attr.value.booldata = true;
+
+    meta = sai_metadata_get_attr_metadata(SAI_OBJECT_TYPE_SWITCH, attr.id);
+
+    EXPECT_EQ(sai_serialize_attr_value(*meta, attr), "true");
+
+    attr.id = SAI_SWITCH_ATTR_ON_LINK_ROUTE_SUPPORTED;
+    attr.value.booldata = false;
+
+    EXPECT_EQ(sai_serialize_attr_value(*meta, attr), "false");
+
+    // deserialize
+
+    attr.id = SAI_SWITCH_ATTR_ON_LINK_ROUTE_SUPPORTED;
+
+    sai_deserialize_attr_value("true", *meta, attr);
+
+    EXPECT_EQ(true, attr.value.booldata);
+
+    sai_deserialize_attr_value("false", *meta, attr);
+
+    EXPECT_EQ(false, attr.value.booldata);
+
+    EXPECT_THROW(sai_deserialize_attr_value("xx", *meta, attr), std::runtime_error);
+}
+
+TEST(SaiSerialize, serialize_chardata)
+{
+    sai_attribute_t attr;
+    const sai_attr_metadata_t* meta;
+    std::string s;
+
+    memset(attr.value.chardata, 0, 32);
+
+    attr.id = SAI_HOSTIF_ATTR_NAME;
+    memcpy(attr.value.chardata, "foo", sizeof("foo"));
+
+    meta = sai_metadata_get_attr_metadata(SAI_OBJECT_TYPE_HOSTIF, attr.id);
+
+    s = sai_serialize_attr_value(*meta, attr);
+
+    EXPECT_EQ(s, "foo");
+
+    attr.id = SAI_HOSTIF_ATTR_NAME;
+    memcpy(attr.value.chardata, "f\\oo\x12", sizeof("f\\oo\x12"));
+
+    meta = sai_metadata_get_attr_metadata(SAI_OBJECT_TYPE_HOSTIF, attr.id);
+
+    s = sai_serialize_attr_value(*meta, attr);
+
+    EXPECT_EQ(s, "f\\\\oo\\x12");
+
+    attr.id = SAI_HOSTIF_ATTR_NAME;
+    memcpy(attr.value.chardata, "\x80\xff", sizeof("\x80\xff"));
+
+    meta = sai_metadata_get_attr_metadata(SAI_OBJECT_TYPE_HOSTIF, attr.id);
+
+    s = sai_serialize_attr_value(*meta, attr);
+
+    EXPECT_EQ(s, "\\x80\\xFF");
+
+    // deserialize
+
+    sai_deserialize_attr_value("f\\\\oo\\x12", *meta, attr);
+
+    SWSS_LOG_NOTICE("des: %s", attr.value.chardata);
+
+    EXPECT_EQ(0, strcmp(attr.value.chardata, "f\\oo\x12"));
+
+    sai_deserialize_attr_value("foo", *meta, attr);
+
+    EXPECT_EQ(0, strcmp(attr.value.chardata, "foo"));
+
+    EXPECT_THROW(sai_deserialize_attr_value("\\x2g", *meta, attr), std::runtime_error);
+
+    EXPECT_THROW(sai_deserialize_attr_value("\\x2", *meta, attr), std::runtime_error);
+        
+    EXPECT_THROW(sai_deserialize_attr_value("\\s45", *meta, attr), std::runtime_error);
+
+    EXPECT_THROW(sai_deserialize_attr_value("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", *meta, attr), std::runtime_error);
+}
+
+TEST(SaiSerialize, serialize_uint64)
+{
+    sai_attribute_t attr;
+    const sai_attr_metadata_t* meta;
+    std::string s;
+
+    attr.id = SAI_SWITCH_ATTR_NV_STORAGE_SIZE;
+    attr.value.u64 = 42;
+
+    meta = sai_metadata_get_attr_metadata(SAI_OBJECT_TYPE_SWITCH, attr.id);
+
+    s = sai_serialize_attr_value(*meta, attr);
+
+    EXPECT_EQ(s, "42");
+
+    attr.value.u64 = 0x87654321aabbccdd;
+
+    attr.id = SAI_SWITCH_ATTR_NV_STORAGE_SIZE;
+
+    meta = sai_metadata_get_attr_metadata(SAI_OBJECT_TYPE_SWITCH, attr.id);
+
+    s = sai_serialize_attr_value(*meta, attr);
+
+    char buf[32];
+    sprintf(buf, "%" PRIu64, attr.value.u64);
+
+    EXPECT_EQ(s, std::string(buf));
+
+    // deserialize
+
+    sai_deserialize_attr_value("12345", *meta, attr);
+
+    EXPECT_EQ(12345, attr.value.u64);
+
+    EXPECT_THROW(sai_deserialize_attr_value("22345235345345345435", *meta, attr), std::runtime_error);
+        
+    EXPECT_THROW(sai_deserialize_attr_value("2a", *meta, attr), std::runtime_error);
+}
+
+TEST(SaiSerialize, serialize_enum)
+{
+    sai_attribute_t attr;
+    const sai_attr_metadata_t* meta;
+    std::string s;
+
+    attr.id = SAI_SWITCH_ATTR_SWITCHING_MODE;
+    attr.value.s32 = SAI_SWITCH_SWITCHING_MODE_STORE_AND_FORWARD;
+
+    meta = sai_metadata_get_attr_metadata(SAI_OBJECT_TYPE_SWITCH, attr.id);
+
+    s = sai_serialize_attr_value(*meta, attr);
+
+    EXPECT_EQ(s, "SAI_SWITCH_SWITCHING_MODE_STORE_AND_FORWARD");
+
+    attr.value.s32 = -1;
+
+    attr.id = SAI_SWITCH_ATTR_SWITCHING_MODE;
+
+    meta = sai_metadata_get_attr_metadata(SAI_OBJECT_TYPE_SWITCH, attr.id);
+
+    s = sai_serialize_attr_value(*meta, attr);
+
+    EXPECT_EQ(s, "-1");
+
+    attr.value.s32 = 100;
+
+    attr.id = SAI_SWITCH_ATTR_SWITCHING_MODE;
+
+    meta = sai_metadata_get_attr_metadata(SAI_OBJECT_TYPE_SWITCH, attr.id);
+
+    s = sai_serialize_attr_value(*meta, attr);
+
+    EXPECT_EQ(s, "100");
+
+    // deserialize
+
+    sai_deserialize_attr_value("12345", *meta, attr);
+
+    EXPECT_EQ(12345, attr.value.s32);
+
+    sai_deserialize_attr_value("-1", *meta, attr);
+
+    EXPECT_EQ(-1, attr.value.s32);
+
+    sai_deserialize_attr_value("SAI_SWITCH_SWITCHING_MODE_STORE_AND_FORWARD", *meta, attr);
+
+    EXPECT_EQ(SAI_SWITCH_SWITCHING_MODE_STORE_AND_FORWARD, attr.value.s32);
+
+    EXPECT_THROW(sai_deserialize_attr_value("foo", *meta, attr), std::runtime_error);
+}
+
+TEST(SaiSerialize, serialize_mac)
+{
+    sai_attribute_t attr;
+    const sai_attr_metadata_t* meta;
+    std::string s;
+
+    attr.id = SAI_SWITCH_ATTR_SRC_MAC_ADDRESS;
+    memcpy(attr.value.mac, "\x01\x22\x33\xaa\xbb\xcc", 6);
+
+    meta = sai_metadata_get_attr_metadata(SAI_OBJECT_TYPE_SWITCH, attr.id);
+
+    s = sai_serialize_attr_value(*meta, attr);
+
+    EXPECT_EQ(s, "01:22:33:AA:BB:CC");
+
+    // deserialize
+
+    sai_deserialize_attr_value("ff:ee:dd:33:44:55", *meta, attr);
+
+    EXPECT_EQ(0, memcmp("\xff\xee\xdd\x33\x44\x55", attr.value.mac, 6));
+
+    EXPECT_THROW(sai_deserialize_attr_value("foo", *meta, attr), std::runtime_error);
+}
+
+TEST(SaiSerialize, serialize_ip_address)
+{
+    sai_attribute_t attr;
+    const sai_attr_metadata_t* meta;
+    std::string s;
+
+    attr.id = SAI_TUNNEL_ATTR_ENCAP_SRC_IP;
+    attr.value.ipaddr.addr_family = SAI_IP_ADDR_FAMILY_IPV4;
+    attr.value.ipaddr.addr.ip4 = htonl(0x0a000015);
+
+    meta = sai_metadata_get_attr_metadata(SAI_OBJECT_TYPE_TUNNEL, attr.id);
+
+    s = sai_serialize_attr_value(*meta, attr);
+
+    EXPECT_EQ(s, "10.0.0.21");
+
+    attr.id = SAI_TUNNEL_ATTR_ENCAP_SRC_IP;
+    attr.value.ipaddr.addr_family = SAI_IP_ADDR_FAMILY_IPV6;
+
+    uint16_t ip6[] = { 0x1111, 0x2222, 0x3333, 0x4444, 0x5555, 0x6666, 0xaaaa, 0xbbbb };
+
+    memcpy(attr.value.ipaddr.addr.ip6, ip6, 16);
+
+    meta = sai_metadata_get_attr_metadata(SAI_OBJECT_TYPE_TUNNEL, attr.id);
+
+    s = sai_serialize_attr_value(*meta, attr);
+
+    EXPECT_EQ(s, "1111:2222:3333:4444:5555:6666:aaaa:bbbb");
+
+    uint16_t ip6a[] = { 0x0100, 0 ,0 ,0 ,0 ,0 ,0 ,0xff00 };
+
+    memcpy(attr.value.ipaddr.addr.ip6, ip6a, 16);
+
+    meta = sai_metadata_get_attr_metadata(SAI_OBJECT_TYPE_TUNNEL, attr.id);
+
+    s = sai_serialize_attr_value(*meta, attr);
+
+    EXPECT_EQ(s, "1::ff");
+
+    uint16_t ip6b[] = { 0, 0 ,0 ,0 ,0 ,0 ,0 ,0x100 };
+
+    memcpy(attr.value.ipaddr.addr.ip6, ip6b, 16);
+
+    meta = sai_metadata_get_attr_metadata(SAI_OBJECT_TYPE_TUNNEL, attr.id);
+
+    s = sai_serialize_attr_value(*meta, attr);
+
+    EXPECT_EQ(s, "::1");
+
+    int k = 100;
+    attr.value.ipaddr.addr_family = (sai_ip_addr_family_t)k;
+
+    EXPECT_THROW(sai_serialize_attr_value(*meta, attr), std::runtime_error);
+
+    // deserialize
+
+    sai_deserialize_attr_value("10.0.0.23", *meta, attr);
+
+    EXPECT_EQ(attr.value.ipaddr.addr.ip4, htonl(0x0a000017));
+    EXPECT_EQ(attr.value.ipaddr.addr_family, SAI_IP_ADDR_FAMILY_IPV4);
+
+    sai_deserialize_attr_value("1::ff", *meta, attr);
+
+    EXPECT_EQ(0, memcmp(attr.value.ipaddr.addr.ip6, ip6a, 16));
+    EXPECT_EQ(attr.value.ipaddr.addr_family, SAI_IP_ADDR_FAMILY_IPV6);
+
+    EXPECT_THROW(sai_deserialize_attr_value("foo", *meta, attr), std::runtime_error);
+}
+
+TEST(SaiSerialize, serialize_uint32_list)
+{
+    sai_attribute_t attr;
+    const sai_attr_metadata_t* meta;
+    std::string s;
+
+    attr.id = SAI_PORT_ATTR_SUPPORTED_SPEED; //SAI_PORT_ATTR_SUPPORTED_HALF_DUPLEX_SPEED;
+
+    uint32_t list[] = {1,2,3,4,5,6,7};
+
+    attr.value.u32list.count = 7;
+    attr.value.u32list.list = NULL;
+
+    meta = sai_metadata_get_attr_metadata(SAI_OBJECT_TYPE_PORT, attr.id);
+
+    s = sai_serialize_attr_value(*meta, attr);
+
+    EXPECT_EQ(s, "7:null");
+
+    attr.value.u32list.list = list;
+
+    meta = sai_metadata_get_attr_metadata(SAI_OBJECT_TYPE_PORT, attr.id);
+
+    s = sai_serialize_attr_value(*meta, attr);
+
+    EXPECT_EQ(s, "7:1,2,3,4,5,6,7");
+
+    attr.value.u32list.count = 0;
+    attr.value.u32list.list = list;
+
+    meta = sai_metadata_get_attr_metadata(SAI_OBJECT_TYPE_PORT, attr.id);
+
+    s = sai_serialize_attr_value(*meta, attr);
+
+    EXPECT_EQ(s, "0:null");
+
+    attr.value.u32list.count = 0;
+    attr.value.u32list.list = NULL;
+
+    meta = sai_metadata_get_attr_metadata(SAI_OBJECT_TYPE_PORT, attr.id);
+
+    s = sai_serialize_attr_value(*meta, attr);
+
+    EXPECT_EQ(s, "0:null");
+
+    memset(&attr, 0, sizeof(attr));
+
+    sai_deserialize_attr_value("7:1,2,3,4,5,6,7", *meta, attr, false);
+
+    EXPECT_EQ(attr.value.u32list.count, 7);
+    EXPECT_EQ(attr.value.u32list.list[0], 1);
+    EXPECT_EQ(attr.value.u32list.list[1], 2);
+    EXPECT_EQ(attr.value.u32list.list[2], 3);
+    EXPECT_EQ(attr.value.u32list.list[3], 4);
+}
+
+TEST(SaiSerialize, serialize_enum_list)
+{
+    sai_attribute_t attr;
+    const sai_attr_metadata_t* meta;
+    std::string s;
+
+    attr.id = SAI_HASH_ATTR_NATIVE_HASH_FIELD_LIST;
+
+    int32_t list[] = {
+             SAI_NATIVE_HASH_FIELD_SRC_IP,
+             SAI_NATIVE_HASH_FIELD_DST_IP,
+             SAI_NATIVE_HASH_FIELD_VLAN_ID,
+             77
+    };
+
+    attr.value.s32list.count = 4;
+    attr.value.s32list.list = NULL;
+
+    meta = sai_metadata_get_attr_metadata(SAI_OBJECT_TYPE_HASH, attr.id);
+
+    s = sai_serialize_attr_value(*meta, attr);
+
+    EXPECT_EQ(s, "4:null");
+
+    attr.value.s32list.list = list;
+
+    meta = sai_metadata_get_attr_metadata(SAI_OBJECT_TYPE_HASH, attr.id);
+
+    s = sai_serialize_attr_value(*meta, attr);
+
+    // or for enum: 4:SAI_NATIVE_HASH_FIELD[SRC_IP,DST_IP,VLAN_ID,77]
+
+    EXPECT_EQ(s, "4:SAI_NATIVE_HASH_FIELD_SRC_IP,SAI_NATIVE_HASH_FIELD_DST_IP,SAI_NATIVE_HASH_FIELD_VLAN_ID,77");
+
+    attr.value.s32list.count = 0;
+    attr.value.s32list.list = list;
+
+    meta = sai_metadata_get_attr_metadata(SAI_OBJECT_TYPE_HASH, attr.id);
+
+    s = sai_serialize_attr_value(*meta, attr);
+
+    EXPECT_EQ(s, "0:null");
+
+    attr.value.s32list.count = 0;
+    attr.value.s32list.list = NULL;
+
+    meta = sai_metadata_get_attr_metadata(SAI_OBJECT_TYPE_HASH, attr.id);
+
+    s = sai_serialize_attr_value(*meta, attr);
+
+    EXPECT_EQ(s, "0:null");
+}
+
+TEST(SaiSerialize, serialize_oid)
+{
+    sai_attribute_t attr;
+    const sai_attr_metadata_t* meta;
+    std::string s;
+
+    attr.id = SAI_SWITCH_ATTR_DEFAULT_VIRTUAL_ROUTER_ID;
+
+    attr.value.oid = 0x1234567890abcdef;
+
+    meta = sai_metadata_get_attr_metadata(SAI_OBJECT_TYPE_SWITCH, attr.id);
+
+    s = sai_serialize_attr_value(*meta, attr);
+
+    EXPECT_EQ(s, "oid:0x1234567890abcdef");
+
+    // deserialize
+
+    sai_deserialize_attr_value("oid:0x1234567890abcdef", *meta, attr);
+
+    EXPECT_EQ(0x1234567890abcdef, attr.value.oid);
+
+    EXPECT_THROW(sai_deserialize_attr_value("foo", *meta, attr), std::runtime_error);
+}
+
+TEST(SaiSerialize, serialize_oid_list)
+{
+    sai_attribute_t attr;
+    const sai_attr_metadata_t* meta;
+    std::string s;
+
+    attr.id = SAI_SWITCH_ATTR_PORT_LIST;
+
+    sai_object_id_t list[] = {
+        1,0x42, 0x77
+    };
+
+    attr.value.objlist.count = 3;
+    attr.value.objlist.list = NULL;
+
+    meta = sai_metadata_get_attr_metadata(SAI_OBJECT_TYPE_SWITCH, attr.id);
+
+    s = sai_serialize_attr_value(*meta, attr);
+
+    EXPECT_EQ(s, "3:null");
+
+    attr.value.objlist.list = list;
+
+    meta = sai_metadata_get_attr_metadata(SAI_OBJECT_TYPE_SWITCH, attr.id);
+
+    s = sai_serialize_attr_value(*meta, attr);
+
+    // or: 4:[ROUTE:0x1,PORT:0x3,oid:0x77] if we have query function
+
+    EXPECT_EQ(s, "3:oid:0x1,oid:0x42,oid:0x77");
+
+    attr.value.objlist.count = 0;
+    attr.value.objlist.list = list;
+
+    meta = sai_metadata_get_attr_metadata(SAI_OBJECT_TYPE_SWITCH, attr.id);
+
+    s = sai_serialize_attr_value(*meta, attr);
+
+    EXPECT_EQ(s, "0:null");
+
+    attr.value.objlist.count = 0;
+    attr.value.objlist.list = NULL;
+
+    meta = sai_metadata_get_attr_metadata(SAI_OBJECT_TYPE_SWITCH, attr.id);
+
+    s = sai_serialize_attr_value(*meta, attr);
+
+    EXPECT_EQ(s, "0:null");
+
+    memset(&attr, 0, sizeof(attr));
+
+    // deserialize
+
+    sai_deserialize_attr_value("3:oid:0x1,oid:0x42,oid:0x77", *meta, attr, false);
+
+    EXPECT_EQ(attr.value.objlist.count, 3);
+    EXPECT_EQ(attr.value.objlist.list[0], 0x1);
+    EXPECT_EQ(attr.value.objlist.list[1], 0x42);
+    EXPECT_EQ(attr.value.objlist.list[2], 0x77);
+}
+
+TEST(SaiSerialize, serialize_acl_action)
+{
+    sai_attribute_t attr;
+    const sai_attr_metadata_t* meta;
+    std::string s;
+
+    attr.id = SAI_ACL_ENTRY_ATTR_ACTION_REDIRECT;
+
+    attr.value.aclaction.enable = true;
+    attr.value.aclaction.parameter.oid = (sai_object_id_t)2;
+
+    meta = sai_metadata_get_attr_metadata(SAI_OBJECT_TYPE_ACL_ENTRY, attr.id);
+
+    s = sai_serialize_attr_value(*meta, attr);
+
+    EXPECT_EQ(s, "oid:0x2");
+
+    attr.value.aclaction.enable = false;
+    attr.value.aclaction.parameter.oid = (sai_object_id_t)2;
+
+    meta = sai_metadata_get_attr_metadata(SAI_OBJECT_TYPE_ACL_ENTRY, attr.id);
+
+    s = sai_serialize_attr_value(*meta, attr);
+
+    EXPECT_EQ(s, "disabled");
+
+    attr.id = SAI_ACL_ENTRY_ATTR_ACTION_PACKET_ACTION;
+
+    attr.value.aclaction.enable = true;
+    attr.value.aclaction.parameter.s32 = SAI_PACKET_ACTION_TRAP;
+
+    meta = sai_metadata_get_attr_metadata(SAI_OBJECT_TYPE_ACL_ENTRY, attr.id);
+
+    s = sai_serialize_attr_value(*meta, attr);
+
+    EXPECT_EQ(s, "SAI_PACKET_ACTION_TRAP");
+
+    attr.value.aclaction.enable = true;
+    attr.value.aclaction.parameter.s32 = 77;
+
+    meta = sai_metadata_get_attr_metadata(SAI_OBJECT_TYPE_ACL_ENTRY, attr.id);
+
+    s = sai_serialize_attr_value(*meta, attr);
+
+    EXPECT_EQ(s, "77");
+}
+
+TEST(SaiSerialize, serialize_qos_map)
+{
+    sai_attribute_t attr;
+    const sai_attr_metadata_t* meta;
+    std::string s;
+
+    attr.id = SAI_QOS_MAP_ATTR_MAP_TO_VALUE_LIST;
+
+    sai_qos_map_t qm = {
+        .key   = { .tc = 1, .dscp = 2, .dot1p = 3, .prio = 4, .pg = 5, .queue_index = 6, .color = SAI_PACKET_COLOR_RED, .mpls_exp = 0 },
+        .value = { .tc = 11, .dscp = 22, .dot1p = 33, .prio = 44, .pg = 55, .queue_index = 66, .color = SAI_PACKET_COLOR_GREEN, .mpls_exp = 0 } };
+
+    attr.value.qosmap.count = 1;
+    attr.value.qosmap.list = &qm;
+
+    meta = sai_metadata_get_attr_metadata(SAI_OBJECT_TYPE_QOS_MAP, attr.id);
+
+    s = sai_serialize_attr_value(*meta, attr);
+
+    std::string ret = "{\"count\":1,\"list\":[{\"key\":{\"color\":\"SAI_PACKET_COLOR_RED\",\"dot1p\":3,\"dscp\":2,\"mpls_exp\":0,\"pg\":5,\"prio\":4,\"qidx\":6,\"tc\":1},\"value\":{\"color\":\"SAI_PACKET_COLOR_GREEN\",\"dot1p\":33,\"dscp\":22,\"mpls_exp\":0,\"pg\":55,\"prio\":44,\"qidx\":66,\"tc\":11}}]}";
+
+    EXPECT_EQ(s, ret);
+
+    s = sai_serialize_attr_value(*meta, attr, true);
+
+    std::string ret2 = "{\"count\":1,\"list\":null}";
+    EXPECT_EQ(s, ret2);
+
+    // deserialize
+
+    memset(&attr, 0, sizeof(attr));
+
+    sai_deserialize_attr_value(ret, *meta, attr);
+
+    EXPECT_EQ(attr.value.qosmap.count, 1);
+
+    auto &l = attr.value.qosmap.list[0];
+
+    EXPECT_EQ(l.key.tc, 1);
+    EXPECT_EQ(l.key.dscp, 2);
+    EXPECT_EQ(l.key.dot1p, 3);
+    EXPECT_EQ(l.key.prio, 4);
+    EXPECT_EQ(l.key.pg, 5);
+    EXPECT_EQ(l.key.queue_index, 6);
+    EXPECT_EQ(l.key.color, SAI_PACKET_COLOR_RED);
+    EXPECT_EQ(l.key.mpls_exp, 0);
+
+    EXPECT_EQ(l.value.tc, 11);
+    EXPECT_EQ(l.value.dscp, 22);
+    EXPECT_EQ(l.value.dot1p, 33);
+    EXPECT_EQ(l.value.prio, 44);
+    EXPECT_EQ(l.value.pg, 55);
+    EXPECT_EQ(l.value.queue_index, 66);
+    EXPECT_EQ(l.value.color, SAI_PACKET_COLOR_GREEN);
+    EXPECT_EQ(l.value.mpls_exp, 0);
+}
+
+template<typename T>
+static void deserialize_number(
+        _In_ const std::string& s,
+        _Out_ T& number,
+        _In_ bool hex = false)
+{
+    SWSS_LOG_ENTER();
+
+    errno = 0;
+
+    char *endptr = NULL;
+
+    number = (T)strtoull(s.c_str(), &endptr, hex ? 16 : 10);
+
+    if (errno != 0 || endptr != s.c_str() + s.length())
+    {
+        SWSS_LOG_THROW("invalid number %s", s.c_str());
+    }
+}
+
+template <typename T>
+static std::string serialize_number(
+        _In_ const T& number,
+        _In_ bool hex = false)
+{
+    SWSS_LOG_ENTER();
+
+    if (hex)
+    {
+        char buf[32];
+
+        snprintf(buf, sizeof(buf), "0x%" PRIx64, (uint64_t)number);
+
+        return buf;
+    }
+
+    return std::to_string(number);
+}
+
+
+TEST(SaiSerialize, serialize_number)
+{
+    SWSS_LOG_ENTER();
+
+    int64_t sp =  0x12345678;
+    int64_t sn = -0x12345678;
+    int64_t u  =  0x12345678;
+
+    auto ssp = serialize_number(sp);
+    auto ssn = serialize_number(sn);
+    auto su  = serialize_number(u);
+
+    EXPECT_EQ(ssp, std::to_string(sp));
+    EXPECT_EQ(ssn, std::to_string(sn));
+    EXPECT_EQ(su,  std::to_string(u));
+
+    auto shsp = serialize_number(sp, true);
+    auto shsn = serialize_number(sn, true);
+    auto shu  = serialize_number(u,  true);
+
+    EXPECT_EQ(shsp, "0x12345678");
+    EXPECT_EQ(shsn, "0xffffffffedcba988");
+    EXPECT_EQ(shu,  "0x12345678");
+
+    sp = 0;
+    sn = 0;
+    u  = 0;
+
+    deserialize_number(ssp, sp);
+    deserialize_number(ssn, sn);
+    deserialize_number(su,  u);
+
+    EXPECT_EQ(sp,  0x12345678);
+    EXPECT_EQ(sn, -0x12345678);
+    EXPECT_EQ(u,   0x12345678);
+
+    deserialize_number(shsp, sp, true);
+    deserialize_number(shsn, sn, true);
+    deserialize_number(shu,  u,  true);
+
+    EXPECT_EQ(sp,  0x12345678);
+    EXPECT_EQ(sn, -0x12345678);
+    EXPECT_EQ(u,   0x12345678);
 }
