433c8dfb0599a0cf774812fdff05a349e9af258b
vmittal-msft <46945843+vmittal-msft@users.noreply.github.com>
2021-10-21 15:41:40 -0700

    [SAI submodule] Updating SAI to v1.9.1 (#955)
    * Updating SAI to v1.9.1

--- tests/checkwhitespace.sh
+++ tests/checkwhitespace.sh
@@ -4,6 +4,7 @@ echo Checkig for white spaces ...
 
 find .. -type f |
 grep -v SAI/ |
+grep -v debian/ |
 grep -v _wrap.cpp |
 perl -ne 'print if /\.(c|cpp|h|hpp|am|sh|pl|pm|install|dirs|links|json|ini|yml|pws|md|py|cfg|conf|i|ac)$/' |
 xargs grep -P "\\s\$"
